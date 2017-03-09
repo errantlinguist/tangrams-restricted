9cf87611ab75b5580dff4c0acffd40762ce0d0e3
Todd Shore <tcshore@kth.se>
2017-03-09 15:07:40 +0100

    added board dimensions-to-matrix-dimensions validation

--- client/src/main/java/se/kth/speech/coin/tangrams/view/GameBoardPanel.java
+++ client/src/main/java/se/kth/speech/coin/tangrams/view/GameBoardPanel.java
@@ -16,12 +16,13 @@
 */
 package se.kth.speech.coin.tangrams.view;
 
+import java.awt.Canvas;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map.Entry;
-
-import javax.swing.JPanel;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,9 +35,14 @@ import se.kth.speech.SpatialMap;
  * @since 2 Mar 2017
  *
  */
-final class GameBoardPanel extends JPanel {
+final class GameBoardPanel extends Canvas {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(GameBoardPanel.class);
+	
+	private static boolean isDimensionDivisibleIntoGrid(Dimension dim, Matrix<?> matrix){
+		int[] matrixDims = matrix.getDimensions();
+		return dim.getHeight() % matrixDims[0] == 0 &&  dim.getWidth() % matrixDims[1] == 0;
+	}
 
 	/**
 	 *
@@ -44,8 +50,18 @@ final class GameBoardPanel extends JPanel {
 	private static final long serialVersionUID = 6258829324465894025L;
 
 	public GameBoardPanel(final Dimension boardSize, List<Entry<BufferedImage, ImageViewInfo>> imgViewInfoDataList, Matrix<Integer> posMatrix, SpatialMap<Entry<BufferedImage, ImageViewInfo>> imagePlacements) {
+		if (!isDimensionDivisibleIntoGrid(boardSize, posMatrix)){
+			throw new IllegalArgumentException(String.format("Board %s not divisble into matrix with dimensions %s.", boardSize, Arrays.toString(posMatrix.getDimensions())));
+		}
 		setSize(boardSize);
 		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see java.awt.Canvas#paint(java.awt.Graphics)
+	 */
+	@Override
+	public void paint(Graphics g) {
 	}
 
 }
