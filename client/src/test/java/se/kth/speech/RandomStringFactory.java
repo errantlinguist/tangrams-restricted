218417c457e0fb1b3581b11efd869e0298a0cd8b
Todd Shore <tcshore@kth.se>
2017-03-10 12:23:23 +0100

    added random URL generation to test case

---
+++ client/src/test/java/se/kth/speech/RandomStringFactory.java
@@ -0,0 +1,66 @@
+/*
+ *  This file is part of client.
+ *
+ *  client is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package se.kth.speech;
+
+import java.util.Random;
+import java.util.function.Supplier;
+
+/**
+ * @author <a href="mailto:tcshore@kth.se">Todd Shore</a>
+ * @since 10 Mar 2017
+ * @see <a href="http://stackoverflow.com/a/41156/1391325">StackOverflow</a>
+ *
+ */
+public final class RandomStringFactory implements Supplier<String> {
+
+	private static final char[] DEFAULT_SYMBOLS = createDefaultSymbolArray();
+
+	private static char[] createDefaultSymbolArray() {
+		final StringBuilder tmp = new StringBuilder();
+		for (char ch = '0'; ch <= '9'; ++ch) {
+			tmp.append(ch);
+		}
+		for (char ch = 'a'; ch <= 'z'; ++ch) {
+			tmp.append(ch);
+		}
+		return tmp.toString().toCharArray();
+	}
+
+	private final char[] buf;
+
+	private final Random random;
+
+	public RandomStringFactory(final int length) {
+		this(new Random(), length);
+	}
+
+	public RandomStringFactory(final Random random, final int length) {
+		this.random = random;
+		if (length < 1) {
+			throw new IllegalArgumentException("length < 1: " + length);
+		}
+		buf = new char[length];
+	}
+
+	@Override
+	public String get() {
+		for (int idx = 0; idx < buf.length; ++idx) {
+			buf[idx] = DEFAULT_SYMBOLS[random.nextInt(DEFAULT_SYMBOLS.length)];
+		}
+		return new String(buf);
+	}
+}
