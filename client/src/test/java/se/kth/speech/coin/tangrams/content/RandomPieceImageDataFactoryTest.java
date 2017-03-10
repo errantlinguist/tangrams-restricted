93768598e5d865216d2f89811ae40b074c170baf
Todd Shore <tcshore@kth.se>
2017-03-10 12:38:55 +0100

    added test for stability across invokations

--- client/src/test/java/se/kth/speech/coin/tangrams/content/RandomPieceImageDataFactoryTest.java
+++ client/src/test/java/se/kth/speech/coin/tangrams/content/RandomPieceImageDataFactoryTest.java
@@ -16,16 +16,17 @@
 */
 package se.kth.speech.coin.tangrams.content;
 
-import static org.junit.Assert.assertEquals;
-
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 
+import org.junit.Assume;
 import org.junit.experimental.theories.DataPoints;
 import org.junit.experimental.theories.Theories;
 import org.junit.experimental.theories.Theory;
 import org.junit.runner.RunWith;
+
+import se.kth.speech.junit.IteratorEqualityAsserter;
 
 /**
  * @author <a href="mailto:tcshore@kth.se">Todd Shore</a>
@@ -35,22 +36,38 @@ import org.junit.runner.RunWith;
 @RunWith(Theories.class)
 public final class RandomPieceImageDataFactoryTest {
 
+	@DataPoints
+	public static final long[] TEST_SEEDS = new Random().longs().distinct().limit(10).toArray();
+
+	private static final IteratorEqualityAsserter<Object> ITER_EQUALITY_ASSERTER = new IteratorEqualityAsserter<>();
+
 	private static final RandomPieceImageDataFactory TEST_FACTORY = new RandomPieceImageDataFactory(
 			IconImages.getIconImageResources().entrySet(), 2);
-
-	@DataPoints
-	public static final Random[] TEST_RANDOMS = new Random().longs().distinct()
-			.mapToObj(Random::new).limit(10).toArray(Random[]::new);
 
 	/**
 	 * Test method for
 	 * {@link se.kth.speech.coin.tangrams.content.RandomPieceImageDataFactory#apply(java.util.Random)}.
 	 */
 	@Theory
-	public final void testApplyUnique(final Random rnd) {
-		final List<?> results = TEST_FACTORY.apply(rnd).collect(Collectors.toList());
-		final List<?> distinctResults = results.stream().distinct().collect(Collectors.toList());
-		assertEquals(distinctResults, results);
+	public final void testApplyStable(final long s1, final long s2) {
+		Assume.assumeTrue(s1 == s2);
+		final Random rnd1 = new Random(s1);
+		final Random rnd2 = new Random(s2);
+		final List<Object> results1 = TEST_FACTORY.apply(rnd1).collect(Collectors.toList());
+		final List<Object> results2 = TEST_FACTORY.apply(rnd2).collect(Collectors.toList());
+		ITER_EQUALITY_ASSERTER.accept(results1.iterator(), results2.iterator());
 	}
-	
+
+	/**
+	 * Test method for
+	 * {@link se.kth.speech.coin.tangrams.content.RandomPieceImageDataFactory#apply(java.util.Random)}.
+	 */
+	@Theory
+	public final void testApplyUnique(final long seed) {
+		final Random rnd = new Random(seed);
+		final List<Object> results = TEST_FACTORY.apply(rnd).collect(Collectors.toList());
+		final List<Object> distinctResults = results.stream().distinct().collect(Collectors.toList());
+		ITER_EQUALITY_ASSERTER.accept(results.iterator(), distinctResults.iterator());
+	}
+
 }
