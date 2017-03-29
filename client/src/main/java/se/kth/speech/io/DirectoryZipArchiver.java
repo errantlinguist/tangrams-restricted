1d7488a4d0ca068940bd07a944f578e6450f85fc
errantlinguist <errantlinguist+github@gmail.com>
2017-03-30 00:13:15 +0200

    updated directory archiver

--- client/src/main/java/se/kth/speech/io/DirectoryZipArchiver.java
+++ client/src/main/java/se/kth/speech/io/DirectoryZipArchiver.java
@@ -14,7 +14,7 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-package se.kth.speech.coin.tangrams.io;
+package se.kth.speech.io;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
@@ -26,7 +26,6 @@ import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.nio.file.attribute.BasicFileAttributeView;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.concurrent.ExecutorService;
 import java.util.function.BiConsumer;
 import java.util.stream.Stream;
 import java.util.zip.Adler32;
@@ -45,71 +44,61 @@ import org.slf4j.LoggerFactory;
  *      Technology Network</a>
  *
  */
-public final class SessionLogDirZipArchiver implements BiConsumer<Path, Path> {
+public final class DirectoryZipArchiver implements BiConsumer<Path, Path> {
 
-	private static final int BUFFER_SIZE = 2048;
+	private static final int BUFFER_SIZE = 8192;
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(SessionLogDirZipArchiver.class);
-
-	private final ExecutorService executorService;
-
-	public SessionLogDirZipArchiver(final ExecutorService executorService) {
-		this.executorService = executorService;
-	}
+	private static final Logger LOGGER = LoggerFactory.getLogger(DirectoryZipArchiver.class);
 
 	@Override
 	public void accept(final Path indir, final Path outfile) throws UncheckedIOException {
-		executorService.submit(() -> {
-			final byte[] buffer = new byte[2048];
+		final byte[] buffer = new byte[BUFFER_SIZE];
 
-			try (OutputStream os = Files.newOutputStream(outfile, StandardOpenOption.CREATE_NEW,
-					StandardOpenOption.WRITE)) {
-				final CheckedOutputStream checksum = new CheckedOutputStream(os, new Adler32());
-				final ZipOutputStream out = new ZipOutputStream(checksum);
+		try (OutputStream os = Files.newOutputStream(outfile, StandardOpenOption.CREATE_NEW,
+				StandardOpenOption.WRITE)) {
+			final CheckedOutputStream checksum = new CheckedOutputStream(os, new Adler32());
+			try (final ZipOutputStream out = new ZipOutputStream(checksum)) {
+				out.setMethod(ZipEntry.DEFLATED);
 				try (Stream<Path> filePaths = Files.walk(indir, FileVisitOption.FOLLOW_LINKS)) {
 					filePaths.filter(Files::isRegularFile).forEach(filePath -> {
 						final Path relIndir = indir.relativize(filePath);
 						final String entryName = relIndir.toString();
 						final ZipEntry entry = new ZipEntry(entryName);
-						entry.setMethod(ZipEntry.DEFLATED);
 						try {
 							out.putNextEntry(entry);
-							entry.setSize(Files.size(filePath));
 							// Write metadata
 							final BasicFileAttributeView attrView = Files.getFileAttributeView(filePath,
 									BasicFileAttributeView.class);
 							if (attrView == null) {
-								LOGGER.debug(
-										"No file attributes found for file at path \"{}\"; Zipped entry won't contain all attributes for it.",
+								LOGGER.warn(
+										"No file attributes found for file at path \"{}\"; Zipped entry might not have correct metadata.",
 										filePath);
-								entry.setComment("File system attributes not available.");
+								entry.setComment("File attributes not available at time of archiving.");
 								entry.setLastModifiedTime(Files.getLastModifiedTime(filePath));
+								entry.setSize(Files.size(filePath));
 							} else {
 								final BasicFileAttributes attrs = attrView.readAttributes();
 								entry.setCreationTime(attrs.creationTime());
 								entry.setLastAccessTime(attrs.lastAccessTime());
 								entry.setLastModifiedTime(attrs.lastModifiedTime());
+								entry.setSize(attrs.size());
 							}
 							// Write file content
 							try (BufferedInputStream instream = new BufferedInputStream(Files.newInputStream(filePath),
 									BUFFER_SIZE)) {
-								int count;
-								while ((count = instream.read(buffer, 0, BUFFER_SIZE)) != -1) {
+								for (int count = instream.read(buffer, 0, BUFFER_SIZE); count > -1; count = instream
+										.read(buffer, 0, BUFFER_SIZE)) {
 									out.write(buffer, 0, count);
 								}
-								// FIXME: The last file in the archive has a
-								// size of "0" and is unreadable
 							}
 						} catch (final IOException e) {
 							throw new UncheckedIOException(e);
 						}
 					});
-				} finally {
-					out.flush();
 				}
-			} catch (final IOException e) {
-				throw new UncheckedIOException(e);
 			}
-		});
+		} catch (final IOException e) {
+			throw new UncheckedIOException(e);
+		}
 	}
 }
