e02abda543a0041dfa3e25b5f6c780891b40c29e
Todd Shore <tcshore@kth.se>
2017-03-31 09:55:46 +0200

    using method reference instead of lambda

--- client/src/main/java/se/kth/speech/coin/tangrams/TangramsClient.java
+++ client/src/main/java/se/kth/speech/coin/tangrams/TangramsClient.java
@@ -189,7 +189,7 @@ public final class TangramsClient implements Runnable {
 
 	private static Options createOptions() {
 		final Options result = new Options();
-		Arrays.stream(Parameter.values()).map(Parameter::get).forEach(opt -> result.addOption(opt));
+		Arrays.stream(Parameter.values()).map(Parameter::get).forEach(result::addOption);
 		return result;
 	}
 
