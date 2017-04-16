dfc239d2ba6825b1c28bef7b73b846352fab87a6
errantlinguist <errantlinguist+github@gmail.com>
2017-04-17 01:57:20 +0200

    now checking for missing "transcription" XML elements

--- analysis/src/main/java/se/kth/speech/coin/tangrams/analysis/SegmentUtteranceFactory.java
+++ analysis/src/main/java/se/kth/speech/coin/tangrams/analysis/SegmentUtteranceFactory.java
@@ -17,11 +17,16 @@
 package se.kth.speech.coin.tangrams.analysis;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import se.kth.speech.hat.xsd.Annotation.Segments.Segment;
+import se.kth.speech.hat.xsd.Transcription;
 import se.kth.speech.hat.xsd.Transcription.T;
 
 public final class SegmentUtteranceFactory implements Function<Segment, List<Utterance>> {
@@ -31,6 +36,8 @@ public final class SegmentUtteranceFactory implements Function<Segment, List<Utt
 	static {
 		DEFAULT_MIN_SEG_SPACING = 1.0f / SegmentTimes.TIME_TO_MILLS_FACTOR;
 	}
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(SegmentUtteranceFactory.class);
 
 	private final float minSegmentSpacing;
 
@@ -44,40 +51,50 @@ public final class SegmentUtteranceFactory implements Function<Segment, List<Utt
 
 	@Override
 	public List<Utterance> apply(final Segment segment) {
-		final List<Utterance> result = new ArrayList<>();
-		final List<Object> children = segment.getTranscription().getSegmentOrT();
-		final Float initialPrevUttEndTime = segment.getStart();
-		assert initialPrevUttEndTime != null;
-		{
-			float prevUttEndTime = initialPrevUttEndTime;
-			List<T> currentTokenSeq = new ArrayList<>();
-			final String parentSegmentId = segment.getId();
-			for (final Object child : children) {
-				if (child instanceof Segment) {
-					final List<Utterance> childUtts = apply((Segment) child);
-					// If there was a contiguous sequence of terminal tokens
-					// preceding this segment, finish building it
-					if (!currentTokenSeq.isEmpty()) {
-						final float nextUttStartTime = childUtts.get(0).getStartTime();
-						result.add(createUtterance(parentSegmentId, currentTokenSeq, prevUttEndTime, nextUttStartTime));
-						currentTokenSeq = new ArrayList<>();
+		final List<Utterance> result;
+		final Transcription transcription = segment.getTranscription();
+		if (transcription == null) {
+			result = Collections.emptyList();
+			LOGGER.debug("Segment \"{}\" has no {} element: {}",
+					new Object[] { segment.getId(), Transcription.class.getSimpleName() });
+		} else {
+			result = new ArrayList<>();
+			final List<Object> children = transcription.getSegmentOrT();
+			final Float initialPrevUttEndTime = segment.getStart();
+			assert initialPrevUttEndTime != null;
+			{
+				float prevUttEndTime = initialPrevUttEndTime;
+				List<T> currentTokenSeq = new ArrayList<>();
+				final String parentSegmentId = segment.getId();
+				for (final Object child : children) {
+					if (child instanceof Segment) {
+						final List<Utterance> childUtts = apply((Segment) child);
+						// If there was a contiguous sequence of terminal tokens
+						// preceding this segment, finish building it
+						if (!currentTokenSeq.isEmpty()) {
+							final float nextUttStartTime = childUtts.get(0).getStartTime();
+							result.add(createUtterance(parentSegmentId, currentTokenSeq, prevUttEndTime,
+									nextUttStartTime));
+							currentTokenSeq = new ArrayList<>();
+						}
+						// Add the newly-created child utterances after adding
+						// the
+						// now-completed terminal sequence
+						result.addAll(childUtts);
+						prevUttEndTime = childUtts.get(childUtts.size() - 1).getEndTime();
+					} else if (child instanceof T) {
+						currentTokenSeq.add((T) child);
+					} else {
+						throw new IllegalArgumentException(String.format(
+								"Could not parse child annotation of type \"%s\".", child.getClass().getName()));
 					}
-					// Add the newly-created child utterances after adding the
-					// now-completed terminal sequence
-					result.addAll(childUtts);
-					prevUttEndTime = childUtts.get(childUtts.size() - 1).getEndTime();
-				} else if (child instanceof T) {
-					currentTokenSeq.add((T) child);
-				} else {
-					throw new IllegalArgumentException(String.format("Could not parse child annotation of type \"%s\".",
-							child.getClass().getName()));
 				}
-			}
-			if (!currentTokenSeq.isEmpty()) {
-				// Add the last token sequence
-				final Float uttEndTime = segment.getEnd();
-				assert uttEndTime != null;
-				result.add(createUtterance(parentSegmentId, currentTokenSeq, prevUttEndTime, uttEndTime));
+				if (!currentTokenSeq.isEmpty()) {
+					// Add the last token sequence
+					final Float uttEndTime = segment.getEnd();
+					assert uttEndTime != null;
+					result.add(createUtterance(parentSegmentId, currentTokenSeq, prevUttEndTime, uttEndTime));
+				}
 			}
 		}
 
