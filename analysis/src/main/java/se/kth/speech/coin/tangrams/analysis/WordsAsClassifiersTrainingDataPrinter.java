9517c132d35cb0f15b818f007e1d7d6784c9243c
errantlinguist <errantlinguist+github@gmail.com>
2017-04-17 02:55:29 +0200

    started WordsAsClassifiersTrainingDataPrinter

---
+++ analysis/src/main/java/se/kth/speech/coin/tangrams/analysis/WordsAsClassifiersTrainingDataPrinter.java
@@ -0,0 +1,146 @@
+/*
+ *  This file is part of se.kth.speech.coin.tangrams-restricted.analysis.
+ *
+ *  tangrams is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package se.kth.speech.coin.tangrams.analysis;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.FileVisitOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.NavigableSet;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import se.kth.speech.coin.tangrams.analysis.vocab.AnnotationVocabularyCollector;
+import se.kth.speech.coin.tangrams.analysis.vocab.HATWordListFactory;
+
+/**
+ * @author <a href="mailto:tcshore@kth.se">Todd Shore</a>
+ * @since Apr 17, 2017
+ * @see <a href="http://anthology.aclweb.org/W/W15/W15-0124.pdf">Casey
+ *      Kennington, Livia Dia, & David Schlangen. &ldquo;A Discriminative Model
+ *      for Perceptually-Grounded Incremental Reference Resolution.&rdquo; In
+ *      <em>Proceedings of IWCS 2015</em><a>.
+ *
+ */
+public final class WordsAsClassifiersTrainingDataPrinter {
+
+	private enum Parameter implements Supplier<Option> {
+		HELP("?") {
+			@Override
+			public Option get() {
+				return Option.builder(optName).longOpt("help").desc("Prints this message.").build();
+			}
+		},
+		INPATH("i") {
+			@Override
+			public Option get() {
+				return Option.builder(optName).longOpt("inpath").desc("The path to the HAT file corpus to process.")
+						.hasArg().argName("path").type(File.class).required().build();
+			}
+		},
+		OUTPATH("o") {
+			@Override
+			public Option get() {
+				return Option.builder(optName).longOpt("outpath").desc("The path to write the training data to.")
+						.hasArg().argName("path").type(File.class).build();
+			}
+		};
+
+		protected final String optName;
+
+		private Parameter(final String optName) {
+			this.optName = optName;
+		}
+
+	}
+
+	private static final Options OPTIONS = createOptions();
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(WordsAsClassifiersTrainingDataPrinter.class);
+
+	public static void main(final String[] args) throws IOException {
+		final CommandLineParser parser = new DefaultParser();
+		try {
+			final CommandLine cl = parser.parse(OPTIONS, args);
+			if (cl.hasOption(Parameter.HELP.optName)) {
+				printHelp();
+			} else {
+				final File inpath = (File) cl.getParsedOptionValue(Parameter.INPATH.optName);
+				LOGGER.info("Reading annotations from \"{}\".", inpath);
+				final HATWordListFactory f = new HATWordListFactory(new AnnotationVocabularyCollector());
+				final NavigableSet<String> wordList;
+				try (final Stream<Path> inpaths = Files.walk(inpath.toPath(), FileVisitOption.FOLLOW_LINKS)) {
+					wordList = f.apply(inpaths);
+				}
+
+				// TODO: finish
+
+				try (final PrintWriter out = parseOutpath(cl)) {
+					// TODO: finish
+				}
+			}
+		} catch (final ParseException e) {
+			System.out.println(String.format("An error occured while parsing the command-line arguments: %s", e));
+			printHelp();
+		}
+	}
+
+	private static Options createOptions() {
+		final Options result = new Options();
+		Arrays.stream(Parameter.values()).map(Parameter::get).forEach(result::addOption);
+		return result;
+	}
+
+	private static PrintWriter parseOutpath(final CommandLine cl) throws ParseException, IOException {
+		final PrintWriter result;
+		final File outfile = (File) cl.getParsedOptionValue(Parameter.OUTPATH.optName);
+		if (outfile == null) {
+			LOGGER.info("No output file path specified; Writing to standard output.");
+			result = new PrintWriter(System.out);
+		} else {
+			LOGGER.info("Output file path is \"{}\".", outfile);
+			result = new PrintWriter(Files.newBufferedWriter(outfile.toPath(), StandardOpenOption.CREATE));
+		}
+		return result;
+	}
+
+	private static void printHelp() {
+		final HelpFormatter formatter = new HelpFormatter();
+		formatter.printHelp(FeatureVectorPrinter.class.getName(), OPTIONS);
+	}
+
+	/**
+	 *
+	 */
+	public WordsAsClassifiersTrainingDataPrinter() {
+	}
+
+}
