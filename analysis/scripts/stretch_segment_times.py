047aaf54615448b7277d5cf6b4dcc1912a35fbe7
Todd Shore <tcshore@kth.se>
2017-04-27 14:48:41 +0200

    now using argparse library

--- analysis/scripts/stretch_segment_times.py
+++ analysis/scripts/stretch_segment_times.py
@@ -27,31 +27,36 @@ def stretch_segment_times(segments, factor):
 		segment.set("end", str(new_end))
 
 if __name__ == '__main__':
+	import argparse
+	import re
 	import sys
-	if len(sys.argv) < 4:
-		print("Usage: %s INFILE SOURCE_ID_TO_CHANGE TIME_COMPRESSION_FACTOR > OUTFILE" % sys.argv[0], file=sys.stderr)
-		sys.exit(64);
-	else:		
-		import re
-		
-		default_namespace = HAT_DATA_NAMESPACE
-		
-		inpath = sys.argv[1]
-		print("Reading \"%s\"." % inpath, file=sys.stderr)
-		parser = AnnotationParser(QNameStringFactory(default_namespace), {None: default_namespace})
-		doc_tree = etree.parse(inpath)
-		infile_datum = parser(doc_tree)
 	
-		seg_id_pattern = re.compile(sys.argv[2])
-		segments = [segment for segment in infile_datum.segment_data.segments_by_id.values() if seg_id_pattern.match(segment.get("source")) is not None]
-		if segments:
-			print("%d segment(s) matching source ID pattern \"%s\"." % (len(segments), seg_id_pattern.pattern), file=sys.stderr) 
-			
-			factor = Decimal(sys.argv[3])
-			print("Stretching times by a factor of %f." % factor, file=sys.stderr)
-			
-			stretch_segment_times(segments, factor)
-			encoding = doc_tree.docinfo.encoding
-			print_etree_to_file(doc_tree, encoding, sys.stdout)
-		else: 
-			raise ValueError("No segments matching source ID pattern \"%s\"." % seg_id_pattern.pattern)
+	parser = argparse.ArgumentParser(description=sys.argv[0])
+	parser.add_argument("-i", '--infile', metavar='PATH', required=True, help='The file to read.')
+	parser.add_argument("-p", '--source-id-pattern', metavar='REGEX', type=re.compile, required=True, help='A regular expression matching the source ID of the segments to change.')
+	parser.add_argument("-f", '--factor', type=Decimal, required=True, help='The amount to stretch the times by.')
+	parser.add_argument("-o", '--outfile', help='The path to write the output to.')
+	
+	args = parser.parse_args()
+	
+	default_namespace = HAT_DATA_NAMESPACE
+	
+	inpath = args.infile
+	print("Reading \"%s\"." % inpath, file=sys.stderr)
+	parser = AnnotationParser(QNameStringFactory(default_namespace), {None: default_namespace})
+	doc_tree = etree.parse(inpath)
+	infile_datum = parser(doc_tree)
+
+	source_id_pattern = args.source_id_pattern
+	segments = [segment for segment in infile_datum.segment_data.segments_by_id.values() if source_id_pattern.match(segment.get("source")) is not None]
+	if segments:
+		print("%d segment(s) matching source ID pattern \"%s\"." % (len(segments), source_id_pattern.pattern), file=sys.stderr) 
+		
+		factor = args.factor
+		print("Stretching times by a factor of %f." % factor, file=sys.stderr)
+		
+		stretch_segment_times(segments, factor)
+		encoding = doc_tree.docinfo.encoding
+		print_etree_to_file(doc_tree, encoding, sys.stdout)
+	else: 
+		raise ValueError("No segments matching source ID pattern \"%s\"." % source_id_pattern.pattern)
