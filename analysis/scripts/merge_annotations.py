5fc372b182d14da6af5889cc772ef24d4f382134
Todd Shore <tcshore@kth.se>
2017-04-27 12:51:18 +0200

    created "print_elem_tree_to_file(..)"

--- analysis/scripts/merge_annotations.py
+++ analysis/scripts/merge_annotations.py
@@ -5,7 +5,9 @@ Created on Apr 3, 2017
 @author: tshore
 '''
 
+import os
 import os.path
+import tempfile
 
 from lxml import etree
 
@@ -31,14 +33,23 @@ def merge_annotations(inpaths, namespace):
 			result.add(next_datum)
 	return result
 
+def print_elem_tree_to_file(elem_tree, encoding, outfile):
+	tmpfile = tempfile.mkstemp(text=True)
+	tmpfile_path = tmpfile[1]
+	try:
+		elem_tree.write(tmpfile_path, encoding=encoding, xml_declaration=True, pretty_print=True)
+		with open(tmpfile_path, 'r', encoding=encoding) as inf:
+			print(inf.read(), file=outfile)
+	finally:
+		os.close(tmpfile[0])
+		os.remove(tmpfile_path)
+
 if __name__ == '__main__':
 	import sys
 	if len(sys.argv) < 2:
 		print("Usage: %s INPUT_PATHS... > OUTFILE" % sys.argv[0], file=sys.stderr)
 		sys.exit(64);
 	else:
-		import os
-		import tempfile
 
 		default_namespace = HAT_DATA_NAMESPACE
 		# http://stackoverflow.com/a/18340978/1391325
@@ -48,16 +59,5 @@ if __name__ == '__main__':
 		result = merge_annotations(inpaths, default_namespace)
 		annot_elem = result.create_xml_element()
 		annot_tree = etree.ElementTree(annot_elem)
-	
-		tmpfile = tempfile.mkstemp(text=True)
-		tmpfile_path = tmpfile[1]
-		try:
-			encoding = result.encoding
-			annot_tree.write(tmpfile_path, encoding=encoding, xml_declaration=True, pretty_print=True)
-			with open(tmpfile_path, 'r', encoding=encoding) as inf:
-				print(inf.read())
-		finally:
-			os.close(tmpfile[0])
-			os.remove(tmpfile_path)
-				
+		print_elem_tree_to_file(annot_tree, result.encoding, sys.stdout)			
 		
