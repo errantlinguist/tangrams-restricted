929afe757b31d7ae40406d305f438b370cb4b3e7
errantlinguist <errantlinguist+github@gmail.com>
2017-04-29 23:58:34 +0200

    added "LoggedEventTimeStretcher"

---
+++ analysis/src/main/java/se/kth/speech/coin/tangrams/iristk/LoggedEventTimeStretcher.java
@@ -0,0 +1,162 @@
+/*
+ *  This file is part of se.kth.speech.coin.tangrams-restricted.analysis.
+ *
+ *  tangrams is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package se.kth.speech.coin.tangrams.iristk;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.sql.Timestamp;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.eclipsesource.json.JsonObject;
+
+import iristk.system.Event;
+import se.kth.speech.coin.tangrams.iristk.io.LoggedEvents;
+
+/**
+ * @author <a href="mailto:tcshore@kth.se">Todd Shore</a>
+ * @since Apr 29, 2017
+ *
+ */
+public final class LoggedEventTimeStretcher {
+
+	private enum Parameter implements Supplier<Option> {
+		HELP("?") {
+			@Override
+			public Option get() {
+				return Option.builder(optName).longOpt("help").desc("Prints this message.").build();
+			}
+		},
+		INPATH("i") {
+			@Override
+			public Option get() {
+				return Option.builder(optName).longOpt("inpath").desc("The path to event log to process.").hasArg()
+						.argName("path").type(File.class).required().build();
+			}
+		},
+		FACTOR("f") {
+			@Override
+			public Option get() {
+				return Option.builder(optName).longOpt("factor").desc("The amount to stretch the times by.").hasArg()
+						.argName("value").type(Number.class).required().build();
+			}
+		},
+		OUTPATH("o") {
+			@Override
+			public Option get() {
+				return Option.builder(optName).longOpt("outpath").desc("The path to write the processed data to.")
+						.hasArg().argName("path").type(File.class).build();
+			}
+		};
+
+		protected final String optName;
+
+		private Parameter(final String optName) {
+			this.optName = optName;
+		}
+	}
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(LoggedEventTimeStretcher.class);
+
+	private static final Options OPTIONS = createOptions();
+
+	public static void main(final String[] args) throws IOException {
+		final CommandLineParser parser = new DefaultParser();
+		try {
+			final CommandLine cl = parser.parse(OPTIONS, args);
+			if (cl.hasOption(Parameter.HELP.optName)) {
+				printHelp();
+			} else {
+				final Path inpath = ((File) cl.getParsedOptionValue(Parameter.INPATH.optName)).toPath();
+				LOGGER.info("Reading annotations from \"{}\".", inpath);
+				final Stream<Event> events = LoggedEvents.parseLoggedEvents(Files.lines(inpath));
+				final double stretchFactor = ((Number) cl.getParsedOptionValue(Parameter.FACTOR.optName)).doubleValue();
+				LOGGER.info("Stretching logged events by a factor of {}.", stretchFactor);
+				final Stream<Event> shiftedEvents = events.map(event -> {
+					final Timestamp timestamp = Timestamp.valueOf(event.getTime());
+					final double newTimeMills = timestamp.getTime() * stretchFactor;
+					timestamp.setTime(Math.round(newTimeMills));
+					event.setTime(timestamp.toString());
+					return event;
+				});
+
+				try (final PrintWriter out = parseOutpath(cl)) {
+					final Iterator<String> eventReprIter = shiftedEvents.map(Event::toJSON).map(JsonObject::toString)
+							.iterator();
+					if (eventReprIter.hasNext()) {
+						out.print(eventReprIter.next());
+						while (eventReprIter.hasNext()) {
+							out.println();
+							out.print(eventReprIter.next());
+						}
+					}
+				}
+			}
+		} catch (final ParseException e) {
+			System.out.println(String.format("An error occured while parsing the command-line arguments: %s", e));
+			printHelp();
+		}
+	}
+
+	private static Options createOptions() {
+		final Options result = new Options();
+		Arrays.stream(Parameter.values()).map(Parameter::get).forEach(result::addOption);
+		return result;
+	}
+
+	private static PrintWriter parseOutpath(final CommandLine cl) throws ParseException, IOException {
+		final PrintWriter result;
+		final File outfile = (File) cl.getParsedOptionValue(Parameter.OUTPATH.optName);
+		if (outfile == null) {
+			LOGGER.info("No output file path specified; Writing to standard output.");
+			result = new PrintWriter(System.out);
+		} else {
+			LOGGER.info("Output file path is \"{}\".", outfile);
+			result = new PrintWriter(Files.newBufferedWriter(outfile.toPath(), StandardOpenOption.CREATE));
+		}
+		return result;
+	}
+
+	private static void printHelp() {
+		final HelpFormatter formatter = new HelpFormatter();
+		formatter.printHelp(LoggedEventTimeStretcher.class.getName(), OPTIONS);
+	}
+
+	/**
+	 *
+	 */
+	public LoggedEventTimeStretcher() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
