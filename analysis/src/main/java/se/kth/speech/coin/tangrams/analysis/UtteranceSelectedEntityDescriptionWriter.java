c6abbda06569a3871bbfb4134fd0ccf043fbf3db
Todd Shore <tcshore@kth.se>
2017-05-09 12:27:08 +0200

    cleanup

--- analysis/src/main/java/se/kth/speech/coin/tangrams/analysis/UtteranceSelectedEntityDescriptionWriter.java
+++ analysis/src/main/java/se/kth/speech/coin/tangrams/analysis/UtteranceSelectedEntityDescriptionWriter.java
@@ -33,7 +33,6 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.EnumSet;
 import java.util.HashSet;
 import java.util.Iterator;
