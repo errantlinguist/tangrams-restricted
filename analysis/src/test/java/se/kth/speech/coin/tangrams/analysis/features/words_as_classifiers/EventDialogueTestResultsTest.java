6bafa365f5818d6b87056d04d36ffaac161d7477
Todd Shore <tcshore@kth.se>
2017-06-01 08:35:19 +0200

    more test iterations

--- analysis/src/test/java/se/kth/speech/coin/tangrams/analysis/features/words_as_classifiers/EventDialogueTestResultsTest.java
+++ analysis/src/test/java/se/kth/speech/coin/tangrams/analysis/features/words_as_classifiers/EventDialogueTestResultsTest.java
@@ -17,9 +17,14 @@
 package se.kth.speech.coin.tangrams.analysis.features.words_as_classifiers;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
 
 import iristk.system.Event;
 import it.unimi.dsi.fastutil.ints.Int2DoubleMap;
@@ -32,51 +37,43 @@ import se.kth.speech.coin.tangrams.analysis.Utterance;
  * @since May 31, 2017
  *
  */
+@RunWith(Theories.class)
 public final class EventDialogueTestResultsTest {
 
-	// {0.08052273635368928=>[5], 0.07303962011080395=>[7],
-	// 0.06956941639116672=>[13], 0.06450970173096632=>[19],
-	// 0.06225250000646863=>[3], 0.05668477140677224=>[2],
-	// 0.053169717542337715=>[12], 0.05107776309599048=>[17],
-	// 0.0492581391364514=>[0], 0.048225258646517645=>[9],
-	// 0.04804251162810409=>[18], 0.04756822650462815=>[15],
-	// 0.04590847090319988=>[14], 0.04159146983372071=>[8],
-	// 0.040567229529046636=>[10], 0.039745500506388455=>[6],
-	// 0.03960019278814138=>[4], 0.03928188187638165=>[11],
-	// 0.035364437818475716=>[1], 0.026732497546490454=>[16]}
+	@DataPoints
+	public static final int[] TESTED_RANKS;
 
-	@Test
-	public void testRank1() {
-		final Int2DoubleMap referentConfidenceVals = new Int2DoubleOpenHashMap();
-		referentConfidenceVals.put(0, 0.23245);
-		referentConfidenceVals.put(1, 0.5343);
-		referentConfidenceVals.put(2, 0.211234);
-		referentConfidenceVals.put(3, 0.04352434);
-		final int goldStandardReferentId = 2;
-		final Utterance testUtt = new Utterance("segment1", "testSpeaker", Arrays.asList("test", "utterance"), 2.3f,
-				3.3f);
-		final EventDialogue transformedDiag = new EventDialogue(Arrays.asList(new Event()), Arrays.asList(testUtt));
-		final int totalDiagUttCount = 1;
-		final EventDialogueTestResults testInst = new EventDialogueTestResults(referentConfidenceVals,
-				goldStandardReferentId, transformedDiag, totalDiagUttCount);
-		Assert.assertEquals(3, testInst.rank(), 0.00001);
+	private static final int[] EXPECTED_ENTITY_ID_RANKING;
+
+	private static final Int2DoubleMap REF_CONF_VALS;
+
+	static {
+		REF_CONF_VALS = new Int2DoubleOpenHashMap();
+		EXPECTED_ENTITY_ID_RANKING = new int[4];
+		REF_CONF_VALS.put(3, 0.23245);
+		EXPECTED_ENTITY_ID_RANKING[1] = 3;
+		REF_CONF_VALS.put(1, 0.5343);
+		EXPECTED_ENTITY_ID_RANKING[0] = 1;
+		REF_CONF_VALS.put(2, 0.211234);
+		EXPECTED_ENTITY_ID_RANKING[2] = 2;
+		REF_CONF_VALS.put(0, 0.04352434);
+		EXPECTED_ENTITY_ID_RANKING[3] = 0;
+		TESTED_RANKS = IntStream.rangeClosed(1, EXPECTED_ENTITY_ID_RANKING.length).toArray();
 	}
 
-	@Test
-	public void testRank2() {
-		final Int2DoubleMap referentConfidenceVals = new Int2DoubleOpenHashMap();
-		referentConfidenceVals.put(0, 0.23245);
-		referentConfidenceVals.put(1, 0.5343);
-		referentConfidenceVals.put(2, 0.211234);
-		referentConfidenceVals.put(3, 0.04352434);
-		final int goldStandardReferentId = 1;
+	private static EventDialogueTestResults createMockDiagTestResult(final int rank) {
+		final int goldStandardReferentId = EXPECTED_ENTITY_ID_RANKING[rank - 1];
 		final Utterance testUtt = new Utterance("segment1", "testSpeaker", Arrays.asList("test", "utterance"), 2.3f,
 				3.3f);
 		final EventDialogue transformedDiag = new EventDialogue(Arrays.asList(new Event()), Arrays.asList(testUtt));
 		final int totalDiagUttCount = 1;
-		final EventDialogueTestResults testInst = new EventDialogueTestResults(referentConfidenceVals,
-				goldStandardReferentId, transformedDiag, totalDiagUttCount);
-		Assert.assertEquals(1, testInst.rank(), 0.00001);
+		return new EventDialogueTestResults(REF_CONF_VALS, goldStandardReferentId, transformedDiag, totalDiagUttCount);
+	}
+
+	@Theory
+	public void testRank(final int testedRank) {
+		final EventDialogueTestResults testInst = createMockDiagTestResult(testedRank);
+		Assert.assertEquals(testedRank, testInst.rank(), 0.00001);
 	}
 
 	@Test
