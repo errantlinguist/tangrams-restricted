ff280c32f99c4c03106b9e5a9d0a7183d4ba7948
errantlinguist <errantlinguist+github@gmail.com>
2017-06-08 21:08:31 +0200

    "read_param_values(..)" -> "read_test_param_values(..)"

--- analysis/scripts/tabular_data/test_param_val_counts.py
+++ analysis/scripts/tabular_data/test_param_val_counts.py
@@ -40,7 +40,7 @@ class ParameterValues(object):
 		subtype_vals = subtypes[param_subtype]
 		subtype_vals[value] += 1
 		
-def read_param_values(infile_paths, param_whitelisting_filter):
+def read_test_param_values(infile_paths, param_whitelisting_filter):
 	result = ParameterValues()
 	for infile_path in infile_paths:
 		print("Reading test parameters from \"%s\"." % infile_path, file=sys.stderr)
@@ -94,7 +94,7 @@ if __name__ == "__main__":
 		print("Will print only parameters which match at least one of the following regexes: %s" % sorted(param_name_regexes), file=sys.stderr)
 		whitelisted_param_pattern = re.compile(__unify_regexes(param_name_regexes))
 		param_whitelisting_filter = lambda param_name: whitelisted_param_pattern.match(param_name) is not None
-		param_vals = read_param_values(infile_paths, param_whitelisting_filter)
+		param_vals = read_test_param_values(infile_paths, param_whitelisting_filter)
 		col_names = ("Parameter", "Subtype", "Value", "Count")
 		print(__COL_DELIM.join(col_names))
 		for param_val_row_count in param_vals.iter_param_val_counts():
