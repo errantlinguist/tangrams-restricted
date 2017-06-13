3fa03bdabf6f97449c0c7e46c0f13205d76d11e9
errantlinguist <errantlinguist+github@gmail.com>
2017-06-13 06:08:53 -0700

    made code object-oriented with class "TestParameterCombinationRankFileReader"

--- analysis/scripts/tabular_data/test_param_combination_ranks.py
+++ analysis/scripts/tabular_data/test_param_combination_ranks.py
@@ -7,28 +7,39 @@ from common import COL_DELIM, RANK_COL_NAME, parse_row_cells
 from test_param_combinations import create_param_whitelisting_filter, create_col_name_idx_map, parse_test_param_subtype_value
 
 
-def read_test_param_combination_ranks(infile_paths, test_param_whitelisting_filter, rank_cell_val_transformer=None):
-	if not rank_cell_val_transformer:
-		rank_cell_val_transformer = lambda rank_cell_value : float(rank_cell_value)
+class TestParameterCombinationRankFileReader(object):
+	
+	def __init__(self, test_param_whitelisting_filter, rank_cell_val_transformer=None):
+		self.test_param_whitelisting_filter = test_param_whitelisting_filter
+		if not rank_cell_val_transformer:
+			rank_cell_val_transformer = lambda rank_cell_value : float(rank_cell_value)
+		self.rank_cell_val_transformer = rank_cell_val_transformer
 		
-	param_combination_ranks = defaultdict(Counter)
-	param_names = set()
-	for infile_path in infile_paths:
-		print("Reading test parameters from \"%s\"." % infile_path, file=sys.stderr)
-		with open(infile_path, 'r') as infile:
-			rows = (parse_row_cells(line) for line in infile)
-			col_names = next(rows)
-			param_col_name_idxs = create_col_name_idx_map(col_names, test_param_whitelisting_filter)
-			param_names.update(param_col_name_idxs.keys())
-			rank_idx = col_names.index(RANK_COL_NAME)
+	def __call__(self, infile_paths):
+		param_combination_ranks = self._create_param_combination_rank_coll()
+		param_names = set()
+		for infile_path in infile_paths:
+			print("Reading test parameters from \"%s\"." % infile_path, file=sys.stderr)
+			with open(infile_path, 'r') as infile:
+				rows = (parse_row_cells(line) for line in infile)
+				col_names = next(rows)
+				param_col_name_idxs = create_col_name_idx_map(col_names, self.test_param_whitelisting_filter)
+				param_names.update(param_col_name_idxs.keys())
+				rank_idx = col_names.index(RANK_COL_NAME)
+				
+				for row in rows:
+					param_vals = tuple((col_name, parse_test_param_subtype_value(row[idx])) for (col_name, idx) in param_col_name_idxs.items())
+					rank = self.rank_cell_val_transformer(row[rank_idx])
+					self._process_param_combination_rank(param_combination_ranks, param_vals, rank)
 			
-			for row in rows:
-				param_vals = tuple((col_name, parse_test_param_subtype_value(row[idx])) for (col_name, idx) in param_col_name_idxs.items())
-				param_combination_rank_counts = param_combination_ranks[param_vals]
-				rank = rank_cell_val_transformer(row[rank_idx])
-				param_combination_rank_counts[rank] += 1
-		
-	return param_combination_ranks, param_names
+		return param_combination_ranks, param_names
+	
+	def _create_param_combination_rank_coll(self):
+		return defaultdict(Counter)
+	
+	def _process_param_combination_rank(self, param_combination_ranks, param_vals, rank):
+		param_combination_rank_counts = param_combination_ranks[param_vals]
+		param_combination_rank_counts[rank] += 1
 	
 if __name__ == "__main__":
 	if len(sys.argv) < 2:
@@ -37,7 +48,8 @@ if __name__ == "__main__":
 		infile_paths = sys.argv[1:2]
 		input_param_name_regexes = sys.argv[2:]
 		param_whitelisting_filter = create_param_whitelisting_filter(input_param_name_regexes)
-		param_combination_ranks, param_names = read_test_param_combination_ranks(infile_paths, param_whitelisting_filter)
+		reader = TestParameterCombinationRankFileReader(param_whitelisting_filter)
+		param_combination_ranks, param_names = reader(infile_paths)
 		param_name_ordering = tuple(sorted(param_names))
 		col_names = []
 		col_names.extend(param_name_ordering)
