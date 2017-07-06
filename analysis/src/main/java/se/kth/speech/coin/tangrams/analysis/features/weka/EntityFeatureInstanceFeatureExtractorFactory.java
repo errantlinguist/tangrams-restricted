c8b4f374f3be5c357a384a6f9a9d7015c672629e
errantlinguist <errantlinguist+github@gmail.com>
2017-07-06 01:35:26 +0100

    removed complicated expression from context XML

---
+++ analysis/src/main/java/se/kth/speech/coin/tangrams/analysis/features/weka/EntityFeatureInstanceFeatureExtractorFactory.java
@@ -0,0 +1,82 @@
+/*
+ *  This file is part of se.kth.speech.coin.tangrams-restricted.analysis.
+ *
+ *  tangrams is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package se.kth.speech.coin.tangrams.analysis.features.weka;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import org.springframework.beans.factory.FactoryBean;
+
+import se.kth.speech.coin.tangrams.analysis.features.EntityFeature;
+import se.kth.speech.coin.tangrams.analysis.features.EntityFeature.Extractor.Context;
+import se.kth.speech.coin.tangrams.content.IconImages;
+import weka.core.Attribute;
+
+/**
+ * @author <a href="mailto:tcshore@kth.se">Todd Shore</a>
+ * @since Jul 6, 2017
+ *
+ */
+public final class EntityFeatureInstanceFeatureExtractorFactory
+		implements FactoryBean<InstanceFeatureExtractor<EntityFeature, EntityFeature.Extractor.Context>> {
+
+	private static InstanceFeatureExtractor<EntityFeature, EntityFeature.Extractor.Context> create() {
+		final EntityFeature.Extractor fExtr = new EntityFeature.Extractor();
+		final Map<EntityFeature, Attribute> fAttrs = EntityFeature.Extractor
+				.createFeatureAttrMap(new ArrayList<>(IconImages.createImageResourceMap().keySet()));
+		return new InstanceFeatureExtractor<>(fExtr, fAttrs);
+	}
+
+	private InstanceFeatureExtractor<EntityFeature, EntityFeature.Extractor.Context> inst = null;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.springframework.beans.factory.FactoryBean#getObject()
+	 */
+	@Override
+	public InstanceFeatureExtractor<EntityFeature, Context> getObject() throws Exception {
+		if (inst == null) {
+			synchronized (this) {
+				if (inst == null) {
+					inst = create();
+				}
+			}
+		}
+		return inst;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.springframework.beans.factory.FactoryBean#getObjectType()
+	 */
+	@Override
+	public Class<?> getObjectType() {
+		return InstanceFeatureExtractor.class;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.springframework.beans.factory.FactoryBean#isSingleton()
+	 */
+	@Override
+	public boolean isSingleton() {
+		return true;
+	}
+}
