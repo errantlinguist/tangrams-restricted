ad8fa4cf8f53e3b2085bd5590b1a618a87bf1d31
Todd Shore <tcshore@kth.se>
2017-08-01 11:19:33 +0200

    added script for easily reproducing results from GLU 2017

---
+++ analysis/scripts/cross_validation_selected.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+CLASSPATH="target/analysis-0.0.1-SNAPSHOT-jar-with-dependencies.jar"
+HEAP_SIZE="5120m"
+MAIN_CLASS="se.kth.speech.coin.tangrams.analysis.features.words_as_classifiers.cross_validation.CombiningBatchJobTestSingleFileWriter"
+
+exit_code=1
+
+USAGE_MSG="Usage: $0 INPATH OUTDIR"
+
+inpath="${1?"${USAGE_MSG}"}"
+outdir="${2?"${USAGE_MSG}"}"
+
+# https://stackoverflow.com/a/3466183/1391325
+case `uname -s` in
+	*CYGWIN*) echo "Converting paths to Windows format."; inpath="`cygpath -w "${inpath}"`"; outdir="`cygpath -w "${outdir}"`" ;;
+esac
+
+echo "Will read files from \"${inpath}\"."
+echo "Output directory is \"${outdir}\"."
+
+java -server -Xmx${HEAP_SIZE} -cp ${CLASSPATH} ${MAIN_CLASS} "${inpath}" -o "${outdir}/trainingtest-cleaning-NPsWithoutPPs-stopwordfilter.tsv" -c "FILLERS DISFLUENCIES DUPLICATES" -tok "NPS_WITHOUT_PPS" -tt "INFLECTED" -tf "STOPWORDS" -tr "ONE_NEG ALL_NEG DIALOGIC" > "${outdir}/trainingtest-cleaning-NPsWithoutPPs-stopwordfilter.out" 2> "${outdir}/trainingtest-cleaning-NPsWithoutPPs-stopwordfilter.err" &&
+java -server -Xmx${HEAP_SIZE} -cp ${CLASSPATH} ${MAIN_CLASS} "${inpath}" -o "${outdir}/trainingtest-cleaning-NPsWithoutPPs.tsv" -c "FILLERS DISFLUENCIES DUPLICATES" -tok "NPS_WITHOUT_PPS" -tt "INFLECTED" -tf "NO_FILTER" -tr "ONE_NEG ALL_NEG DIALOGIC" > "${outdir}/trainingtest-cleaning-NPsWithoutPPs.out" 2> "${outdir}/trainingtest-cleaning-NPsWithoutPPs.err" &&
+java -server -Xmx${HEAP_SIZE} -cp ${CLASSPATH} ${MAIN_CLASS} "${inpath}" -o "${outdir}/trainingtest-basic.tsv" -c "" -tok "BASIC" -tt "INFLECTED" -tf "NO_FILTER" -tr "ONE_NEG ALL_NEG DIALOGIC" > "${outdir}/trainingtest-basic.out" 2> "${outdir}/trainingtest-basic.err" &&
+java -server -Xmx${HEAP_SIZE} -cp ${CLASSPATH} ${MAIN_CLASS} "${inpath}" -o "${outdir}/trainingtest-basic-stopwordfillers.tsv" -c "FILLERS" -tok "BASIC" -tt "INFLECTED" -tf "STOPWORDS" -tr "ONE_NEG ALL_NEG DIALOGIC" > "${outdir}/trainingtest-basic-stopwordfillers.out" 2> "${outdir}/trainingtest-basic-stopwordfillers.err"
