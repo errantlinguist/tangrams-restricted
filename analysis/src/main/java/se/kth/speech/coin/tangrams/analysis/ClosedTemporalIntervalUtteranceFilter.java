92eb06b70bcf6520c4efaf7eaabbe3cc9312010b
Todd Shore <tcshore@kth.se>
2017-08-02 20:15:33 +0200

    created SessionStatisticsWriter

---
+++ analysis/src/main/java/se/kth/speech/coin/tangrams/analysis/ClosedTemporalIntervalUtteranceFilter.java
@@ -0,0 +1,127 @@
+/*
+ *  This file is part of se.kth.speech.coin.tangrams-restricted.analysis.
+ *
+ *  se.kth.speech.coin.tangrams-restricted.analysis is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package se.kth.speech.coin.tangrams.analysis;
+
+import java.time.LocalDateTime;
+import java.util.function.Predicate;
+
+import se.kth.speech.TimestampArithmetic;
+
+/**
+ * @author <a href="mailto:tcshore@kth.se">Todd Shore</a>
+ * @since 2 Aug 2017
+ *
+ */
+public final class ClosedTemporalIntervalUtteranceFilter implements Predicate<Utterance> {
+
+	private final LocalDateTime end;
+
+	private final LocalDateTime start;
+
+	public ClosedTemporalIntervalUtteranceFilter(final LocalDateTime start, final LocalDateTime end) {
+		if (start.isAfter(end)) {
+			throw new IllegalArgumentException("Start time cannot be after end time.");
+		}
+		this.start = start;
+		this.end = end;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof ClosedTemporalIntervalUtteranceFilter)) {
+			return false;
+		}
+		final ClosedTemporalIntervalUtteranceFilter other = (ClosedTemporalIntervalUtteranceFilter) obj;
+		if (end == null) {
+			if (other.end != null) {
+				return false;
+			}
+		} else if (!end.equals(other.end)) {
+			return false;
+		}
+		if (start == null) {
+			if (other.start != null) {
+				return false;
+			}
+		} else if (!start.equals(other.start)) {
+			return false;
+		}
+		return true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (end == null ? 0 : end.hashCode());
+		result = prime * result + (start == null ? 0 : start.hashCode());
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see java.util.function.Predicate#test(java.lang.Object)
+	 */
+	@Override
+	public boolean test(final Utterance utt) {
+		final LocalDateTime uttStart = TimestampArithmetic.createOffsetTimestamp(start, utt.getStartTime());
+		final boolean uttStartedWithinTimeframe = uttStart.compareTo(start) >= 0;
+
+		final boolean result;
+		if (uttStartedWithinTimeframe) {
+			final LocalDateTime uttEnd = TimestampArithmetic.createOffsetTimestamp(start, utt.getEndTime());
+			result = uttEnd.compareTo(end) <= 0;
+		} else {
+			result = false;
+		}
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		final StringBuilder builder = new StringBuilder();
+		builder.append("ClosedTemporalIntervalUtteranceFilter [start=");
+		builder.append(start);
+		builder.append(", end=");
+		builder.append(end);
+		builder.append("]");
+		return builder.toString();
+	}
+
+}
---
+++ analysis/src/main/java/se/kth/speech/coin/tangrams/analysis/SessionStatisticsWriter.java
@@ -0,0 +1,319 @@
+/*
+ *  This file is part of se.kth.speech.coin.tangrams-restricted.analysis.
+ *
+ *  se.kth.speech.coin.tangrams-restricted.analysis is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package se.kth.speech.coin.tangrams.analysis;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.nio.file.FileVisitOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.NavigableMap;
+import java.util.Properties;
+import java.util.TreeMap;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import iristk.system.Event;
+import se.kth.speech.coin.tangrams.iristk.EventTypeMatcher;
+import se.kth.speech.coin.tangrams.iristk.GameManagementEvent;
+import se.kth.speech.coin.tangrams.iristk.io.HatIO;
+import se.kth.speech.coin.tangrams.iristk.io.LoggedEvents;
+import se.kth.speech.hat.xsd.Annotation;
+import se.kth.speech.hat.xsd.Annotation.Segments.Segment;
+
+/**
+ * @author <a href="mailto:tcshore@kth.se">Todd Shore</a>
+ * @since 2 Aug 2017
+ *
+ */
+final class SessionStatisticsWriter {
+
+	private static class GameSummary {
+
+		private final Duration duration;
+
+		private final long roundCount;
+
+		private final long uttCount;
+
+		private GameSummary(final long roundCount, final Duration duration, final long uttCount) {
+			this.roundCount = roundCount;
+			this.duration = duration;
+			this.uttCount = uttCount;
+		}
+
+		/**
+		 * @return the duration
+		 */
+		public Duration getDuration() {
+			return duration;
+		}
+
+		/**
+		 * @return the roundCount
+		 */
+		public long getRoundCount() {
+			return roundCount;
+		}
+
+		/**
+		 * @return the uttCount
+		 */
+		public long getUttCount() {
+			return uttCount;
+		}
+	}
+
+	private static final List<String> COLUMN_HEADERS = Arrays.asList("INPATH", "GAME_ID", "DURATION", "ROUND_COUNT",
+			"UTT_COUNT");
+
+	private static final ThreadLocal<Unmarshaller> HAT_UNMARSHALLER = new ThreadLocal<Unmarshaller>() {
+
+		/*
+		 * (non-Javadoc)
+		 *
+		 * @see java.lang.ThreadLocal#initialValue()
+		 */
+		@Override
+		protected Unmarshaller initialValue() {
+			try {
+				return HatIO.fetchContext().createUnmarshaller();
+			} catch (final JAXBException e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+	};
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(SessionStatisticsWriter.class);
+
+	private static final MathContext MEAN_DIV_CTX = MathContext.DECIMAL64;
+
+	private static final BigDecimal NANOS_TO_SECS_DIVISOR = new BigDecimal("1000000000");
+
+	private static final EventTypeMatcher NEXT_TURN_REQUEST_EVENT_MATCHER = new EventTypeMatcher(
+			GameManagementEvent.NEXT_TURN_REQUEST);
+
+	private static final Collector<CharSequence, ?, String> ROW_CELL_JOINER = Collectors.joining("\t");
+
+	private static final String ROW_DELIMITER = System.lineSeparator();
+
+	/**
+	 * @param args
+	 * @throws IOException
+	 * @throws JAXBException
+	 */
+	public static void main(final String[] args) throws JAXBException, IOException {
+		final Path[] inpaths = Arrays.stream(args).map(String::trim).filter(path -> !path.isEmpty()).map(Paths::get)
+				.toArray(Path[]::new);
+		if (inpaths.length < 1) {
+			throw new IllegalArgumentException(
+					String.format("Usage: %s INPATHS...", SessionStatisticsWriter.class.getSimpleName()));
+		} else {
+			final NavigableMap<Path, NavigableMap<String, GameSummary>> sessionSummaries = new TreeMap<>();
+			for (final Path inpath : inpaths) {
+				LOGGER.info("Will read batch job data from \"{}\".", inpath);
+				putSessionSummaries(inpath, sessionSummaries);
+			}
+			write(sessionSummaries.entrySet(), System.out);
+		}
+
+	}
+
+	private static GameSummary createGameSummary(final GameHistory history, final List<Utterance> utts) {
+		final Stream<Event> events = history.getEventSequence();
+		final Stream<Event> nextTurnRequestEvents = events.filter(NEXT_TURN_REQUEST_EVENT_MATCHER);
+		// Remove one from count because the last round of the game is never
+		// actually finished
+		final long roundCount = nextTurnRequestEvents.count() - 1;
+		final LocalDateTime gameStart = history.getStartTime();
+		final LocalDateTime lastTurnRequestTime = history.getEvents().lastKey();
+		final Duration duration = Duration.between(gameStart, lastTurnRequestTime);
+		final long uttCount = getUtterancesBetween(utts, gameStart, lastTurnRequestTime).count();
+		assert uttCount > 0;
+		return new GameSummary(roundCount, duration, uttCount);
+	}
+
+	private static NavigableMap<String, GameSummary> createGameSummaryMap(final Path eventLogPath,
+			final List<Utterance> utts) throws IOException {
+		final Map<String, GameHistory> gameHistories = LoggedEvents.readGameHistories(eventLogPath);
+		final NavigableMap<String, GameSummary> result = new TreeMap<>();
+		for (final Entry<String, GameHistory> gameHistory : gameHistories.entrySet()) {
+			final String gameId = gameHistory.getKey();
+			LOGGER.debug("Processing game \"{}\".", gameId);
+			final GameHistory history = gameHistory.getValue();
+			final GameSummary summary = createGameSummary(history, utts);
+			result.put(gameId, summary);
+		}
+		return result;
+	}
+
+	private static NavigableMap<String, GameSummary> createSessionSummary(final SessionDataManager sessionData)
+			throws JAXBException, IOException {
+		final List<Utterance> utts = createUtteranceList(sessionData);
+		final Path eventLogPath = sessionData.getCanonicalEventLogPath();
+		LOGGER.debug("Reading event log at \"{}\".", eventLogPath);
+		return createGameSummaryMap(eventLogPath, utts);
+	}
+
+	private static List<Utterance> createUtteranceList(final SessionDataManager sessionData)
+			throws JAXBException, IOException {
+		final PlayerDataManager playerData = sessionData.getPlayerData();
+		final Map<String, String> sourcePlayerIds = playerData.getPlayerSourceIds().inverse();
+		final SegmentUtteranceFactory segUttFactory = new SegmentUtteranceFactory(seg -> {
+			final String sourceId = seg.getSource();
+			return sourcePlayerIds.get(sourceId);
+		});
+		final Path hatInfilePath = sessionData.getHATFilePath();
+		LOGGER.debug("Reading HAT annotations at \"{}\".", hatInfilePath);
+		final Annotation uttAnnots = readAnnotations(hatInfilePath);
+		final List<Segment> segs = uttAnnots.getSegments().getSegment();
+		return Arrays.asList(segUttFactory.create(segs.stream()).flatMap(List::stream).toArray(Utterance[]::new));
+	}
+
+	private static Stream<GameSummary> getGameSummaries(
+			final Collection<? extends Entry<Path, ? extends Map<String, GameSummary>>> sessionSummaries) {
+		return sessionSummaries.stream().map(Entry::getValue).map(Map::values).flatMap(Collection::stream);
+	}
+
+	private static Stream<Utterance> getUtterancesBetween(final List<Utterance> utts, final LocalDateTime start,
+			final LocalDateTime end) {
+		final ClosedTemporalIntervalUtteranceFilter temporalFilter = new ClosedTemporalIntervalUtteranceFilter(start, end);
+		return utts.stream().filter(temporalFilter);
+	}
+
+	private static void putSessionSummaries(final Path inpath,
+			final Map<? super Path, ? super NavigableMap<String, GameSummary>> sessionSummaries)
+			throws JAXBException, IOException {
+		final Path[] infilePaths = Files.walk(inpath, FileVisitOption.FOLLOW_LINKS).filter(Files::isRegularFile)
+				.filter(filePath -> filePath.getFileName().toString().endsWith(".properties")).toArray(Path[]::new);
+		for (final Path infilePath : infilePaths) {
+			LOGGER.info("Reading batch job properties from \"{}\".", infilePath);
+			final Properties props = new Properties();
+			try (final InputStream propsInstream = Files.newInputStream(infilePath)) {
+				props.load(propsInstream);
+			}
+			final SessionDataManager sessionData = SessionDataManager.create(infilePath);
+			final NavigableMap<String, GameSummary> sessionSummary = createSessionSummary(sessionData);
+			sessionSummaries.put(infilePath, sessionSummary);
+		}
+	}
+
+	private static Annotation readAnnotations(final Path hatInfilePath) throws JAXBException, IOException {
+		try (InputStream instream = Files.newInputStream(hatInfilePath)) {
+			return (Annotation) HAT_UNMARSHALLER.get().unmarshal(instream);
+		}
+	}
+
+	private static BigDecimal toSeconds(final Duration duration) {
+		final BigDecimal nanos = new BigDecimal(duration.toNanos());
+		return nanos.divide(NANOS_TO_SECS_DIVISOR, MathContext.UNLIMITED);
+	}
+
+	private static void write(
+			final Collection<? extends Entry<Path, ? extends Map<String, GameSummary>>> sessionSummaries,
+			final PrintStream out) {
+		out.print(COLUMN_HEADERS.stream().collect(ROW_CELL_JOINER));
+
+		for (final Entry<Path, ? extends Map<String, GameSummary>> sessionSummary : sessionSummaries) {
+			out.print(ROW_DELIMITER);
+
+			final Path inpath = sessionSummary.getKey();
+			final Map<String, GameSummary> gameSummaries = sessionSummary.getValue();
+			for (final Entry<String, GameSummary> gameSummary : gameSummaries.entrySet()) {
+				final String gameId = gameSummary.getKey();
+				final GameSummary summary = gameSummary.getValue();
+				final BigDecimal durationInSecs = toSeconds(summary.getDuration());
+				final List<Object> rowCellVals = Arrays.asList(inpath, gameId, durationInSecs, summary.getRoundCount(),
+						summary.getUttCount());
+				out.print(rowCellVals.stream().map(Object::toString).collect(ROW_CELL_JOINER));
+			}
+
+		}
+
+		out.print(ROW_DELIMITER);
+		writeAggregates(sessionSummaries, out);
+	}
+
+	private static void writeAggregates(
+			final Collection<? extends Entry<Path, ? extends Map<String, GameSummary>>> sessionSummaries,
+			final PrintStream out) {
+		{
+			// Min vals
+			final BigDecimal minDuration = getGameSummaries(sessionSummaries).map(GameSummary::getDuration)
+					.min(Comparator.naturalOrder()).map(SessionStatisticsWriter::toSeconds).get();
+			final long minRoundCount = getGameSummaries(sessionSummaries).mapToLong(GameSummary::getRoundCount).min()
+					.getAsLong();
+			final long minUttCount = getGameSummaries(sessionSummaries).mapToLong(GameSummary::getUttCount).min()
+					.getAsLong();
+			final List<Object> minVals = Arrays.asList("MIN", "", minDuration, minRoundCount, minUttCount);
+			out.print(minVals.stream().map(Object::toString).collect(ROW_CELL_JOINER));
+			out.print(ROW_DELIMITER);
+		}
+
+		{
+			// Max vals
+			final BigDecimal maxDuration = getGameSummaries(sessionSummaries).map(GameSummary::getDuration)
+					.max(Comparator.naturalOrder()).map(SessionStatisticsWriter::toSeconds).get();
+			final long maxRoundCount = getGameSummaries(sessionSummaries).mapToLong(GameSummary::getRoundCount).max()
+					.getAsLong();
+			final long maxUttCount = getGameSummaries(sessionSummaries).mapToLong(GameSummary::getUttCount).max()
+					.getAsLong();
+			final List<Object> maxVals = Arrays.asList("MAX", "", maxDuration, maxRoundCount, maxUttCount);
+			out.print(maxVals.stream().map(Object::toString).collect(ROW_CELL_JOINER));
+			out.print(ROW_DELIMITER);
+		}
+
+		{
+			// Mean vals
+			final BigDecimal gameSessionCount = new BigDecimal(getGameSummaries(sessionSummaries).count());
+			final BigDecimal totalDuration = getGameSummaries(sessionSummaries).map(GameSummary::getDuration)
+					.map(SessionStatisticsWriter::toSeconds).reduce((augend, addend) -> augend.add(addend)).get();
+			final BigDecimal meanDuration = totalDuration.divide(gameSessionCount, MEAN_DIV_CTX);
+			final long totalRoundCount = getGameSummaries(sessionSummaries).mapToLong(GameSummary::getRoundCount).sum();
+			final BigDecimal meanRoundCount = new BigDecimal(totalRoundCount).divide(gameSessionCount, MEAN_DIV_CTX);
+			final long totalUttCount = getGameSummaries(sessionSummaries).mapToLong(GameSummary::getUttCount).sum();
+			final BigDecimal meanUttCount = new BigDecimal(totalUttCount).divide(gameSessionCount, MEAN_DIV_CTX);
+			final List<Object> meanVals = Arrays.asList("MEAN", "", meanDuration, meanRoundCount, meanUttCount);
+			out.print(meanVals.stream().map(Object::toString).collect(ROW_CELL_JOINER));
+			out.print(ROW_DELIMITER);
+		}
+	}
+
+	private SessionStatisticsWriter() {
+	}
+
+}
