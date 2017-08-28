a5d2d06bc49cb328a4dc093a5843965d17907973
Todd Shore <tcshore@kth.se>
2017-08-28 11:03:04 +0200

    removed unused method

--- analysis/src/main/java/se/kth/speech/coin/tangrams/analysis/features/words_as_classifiers/WordClasses.java
+++ analysis/src/main/java/se/kth/speech/coin/tangrams/analysis/features/words_as_classifiers/WordClasses.java
@@ -16,9 +16,6 @@
 */
 package se.kth.speech.coin.tangrams.analysis.features.words_as_classifiers;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 /**
  * @author <a href="mailto:tcshore@kth.se">Todd Shore</a>
  * @since May 26, 2017
@@ -26,28 +23,28 @@ import java.util.regex.Pattern;
  */
 public final class WordClasses {
 
-	private static final Pattern CLASS_RELATION_NAME_PATTERN;
+//	private static final Pattern CLASS_RELATION_NAME_PATTERN;
 
 	private static final String CLASS_RELATION_PREFIX;
 
 	static {
 		CLASS_RELATION_PREFIX = "referent_for_token-";
-		CLASS_RELATION_NAME_PATTERN = Pattern.compile(Pattern.quote(CLASS_RELATION_PREFIX) + "(.+)");
+//		CLASS_RELATION_NAME_PATTERN = Pattern.compile(Pattern.quote(CLASS_RELATION_PREFIX) + "(.+)");
 	}
 
 	public static String createRelationName(final String className) {
 		return CLASS_RELATION_PREFIX + className;
 	}
 
-	public static String parseRelationClassName(final String relName) {
-		final Matcher classRelNameMatcher = CLASS_RELATION_NAME_PATTERN.matcher(relName);
-		if (classRelNameMatcher.matches()) {
-			return classRelNameMatcher.group(1);
-		} else {
-			throw new IllegalArgumentException(
-					String.format("Could not parse a class name from relation name \"%s\".", relName));
-		}
-	}
+//	public static String parseRelationClassName(final String relName) {
+//		final Matcher classRelNameMatcher = CLASS_RELATION_NAME_PATTERN.matcher(relName);
+//		if (classRelNameMatcher.matches()) {
+//			return classRelNameMatcher.group(1);
+//		} else {
+//			throw new IllegalArgumentException(
+//					String.format("Could not parse a class name from relation name \"%s\".", relName));
+//		}
+//	}
 
 	private WordClasses() {
 
