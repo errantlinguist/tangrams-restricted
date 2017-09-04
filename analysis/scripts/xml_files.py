6a1481b17355239724631ec7679e8db5c150ab07
Todd Shore <tcshore@kth.se>
2017-09-04 10:30:07 +0200

    removed unused loop variable

--- analysis/scripts/xml_files.py
+++ analysis/scripts/xml_files.py
@@ -7,7 +7,7 @@ XML_CONTENT_TYPE_PATTERN = re.compile(".*?/xml")
 
 def walk_xml_files(inpaths):
 	for inpath in inpaths:
-		for dirpath, dirnames, filenames in os.walk(inpath, followlinks=True):
+		for dirpath, _, filenames in os.walk(inpath, followlinks=True):
 			for filename in filenames:
 				resolved_path = os.path.join(dirpath, filename)
 				mimetype = mimetypes.guess_type(resolved_path)[0]
