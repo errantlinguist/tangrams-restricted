9ebb5dd8b8ac3b07e548fbe09c510c0083de10ee
Todd Shore <tcshore@kth.se>
2017-09-05 11:07:20 +0200

    preventing loss of precision during averaging

--- analysis/scripts/re_token_group_freqs.py
+++ analysis/scripts/re_token_group_freqs.py
@@ -3,7 +3,7 @@
 import itertools
 import sys
 from collections import Counter
-from decimal import Decimal, ROUND_HALF_UP, getcontext
+from decimal import Decimal, ROUND_HALF_UP, Context, localcontext
 
 from re_token_group_counts import read_annot_token_group_counts, read_token_group_dict
 from xml_files import walk_xml_files
@@ -11,7 +11,7 @@ from xml_files import walk_xml_files
 COL_DELIM = '\t'
 
 
-def print_tabular_freqs(infile_token_group_counts, group_count_sums, file):
+def print_tabular_freqs(infile_token_group_counts, group_count_sums, decimal_printing_ctx, file):
 	item_key_getter = lambda item: item[0]
 	ordered_group_counts = tuple(
 		(group, Decimal(count)) for group, count in sorted(group_count_sums.items(), key=item_key_getter))
@@ -23,14 +23,16 @@ def print_tabular_freqs(infile_token_group_counts, group_count_sums, file):
 		counts = tuple(Decimal(token_group_counts.get(group, 0)) for group in ordered_groups)
 		dyad_total_count = Decimal(sum(counts))
 		freqs = (count / dyad_total_count for count in counts)
-		print(COL_DELIM.join(itertools.chain((infile,), (str(freq) for freq in freqs))),
-			  file=file)
+		with localcontext(decimal_printing_ctx) as _:
+			print(COL_DELIM.join(itertools.chain((infile,), (str(freq) for freq in freqs))),
+				  file=file)
 
 	summary_counts = tuple(count for (_, count) in ordered_group_counts)
 	summary_total_count = sum(summary_counts)
 	summary_freqs = (count / summary_total_count for count in summary_counts)
 	summary_row_cells = itertools.chain(("TOTAL",), (str(freq) for freq in summary_freqs))
-	print(COL_DELIM.join(summary_row_cells))
+	with localcontext(decimal_printing_ctx) as _:
+		print(COL_DELIM.join(summary_row_cells))
 
 
 if __name__ == "__main__":
@@ -44,14 +46,13 @@ if __name__ == "__main__":
 
 		inpaths = sys.argv[2:]
 		infiles = walk_xml_files(*inpaths)
-		infile_token_group_counts = dict((infile, read_annot_token_group_counts(infile, token_groups)) for infile in infiles)
+		infile_token_group_counts = dict(
+			(infile, read_annot_token_group_counts(infile, token_groups)) for infile in infiles)
 
 		group_count_sums = Counter()
 		for group_counts in infile_token_group_counts.values():
 			for group, count in group_counts.items():
 				group_count_sums[group] += count
 
-		ctx = getcontext()
-		ctx.prec = 3
-		ctx.rounding = ROUND_HALF_UP
-		print_tabular_freqs(infile_token_group_counts, group_count_sums, sys.stdout)
+		printing_ctx = Context(prec=3, rounding=ROUND_HALF_UP)
+		print_tabular_freqs(infile_token_group_counts, group_count_sums, printing_ctx, sys.stdout)
