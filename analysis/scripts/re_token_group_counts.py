f99940becc75520f865cdfe85596790a08a20602
Todd Shore <tcshore@kth.se>
2017-09-06 11:42:40 +0200

    fixed type hint

--- analysis/scripts/re_token_group_counts.py
+++ analysis/scripts/re_token_group_counts.py
@@ -5,7 +5,7 @@ import itertools
 import sys
 import xml.etree.ElementTree
 from collections import Counter
-from typing import Dict, FrozenSet, Iterable, Iterator, TextIO, Tuple
+from typing import Counter, Dict, FrozenSet, Iterable, Iterator, TextIO, Tuple
 
 from annotations import ANNOTATION_NAMESPACES
 from xml_files import walk_xml_files
@@ -19,7 +19,7 @@ class TokenGroupDataColumn(object):
 	TOKEN = "TOKEN"
 
 
-def create_annot_token_group_counts(token_annots, token_groups: Dict[str, Iterable[str]]) -> Counter:
+def create_annot_token_group_counts(token_annots, token_groups: Dict[str, Iterable[str]]) -> Counter[str]:
 	result = Counter()
 
 	tokens = (annot.text for annot in token_annots)
