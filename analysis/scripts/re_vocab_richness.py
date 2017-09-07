88e4072ba6b1bdf31af4d443b48854017e0ead89
errantlinguist <errantlinguist+github@gmail.com>
2017-09-07 19:18:34 +0200

    added ability to print out bare token-type counts

--- analysis/scripts/re_vocab_richness.py
+++ analysis/scripts/re_vocab_richness.py
@@ -58,6 +58,17 @@ class SessionRoundTokenCounter(object):
 		return result
 
 
+class TokenTypeCount(object):
+	def __init__(self, tokens, types):
+		if tokens < types:
+			raise ValueError("Token count ({}) is less than type count ({}).".format(tokens, types))
+		self.tokens = tokens
+		self.types = types
+
+	def __repr__(self, *args, **kwargs):
+		return self.__class__.__name__ + str(self.__dict__)
+
+
 def find_falsey_tail_start_idx(elems: Sequence[T]):
 	reversed_elems = reversed(elems)
 	max_valid_idx = len(elems) - 1
@@ -71,7 +82,7 @@ def game_round_token_type_counts(round_token_counts: Iterable[Dict[str, int]]) -
 		total_token_counts.update(token_counts)
 		token_count = sum(total_token_counts.values())
 		type_count = len(total_token_counts.keys())
-		round_token_type_counts.append((token_count, type_count))
+		round_token_type_counts.append(TokenTypeCount(token_count, type_count))
 
 	return RoundTokenTypeData(round_token_type_counts, total_token_counts)
 
@@ -85,7 +96,7 @@ def session_round_token_type_data(named_sessions, relevant_tokens: Container[str
 		if falsey_tail_start_idx:
 			old_len = len(round_token_counts)
 			round_token_counts = round_token_counts[:falsey_tail_start_idx + 1]
-			print("Trimmed {} empty round(s).".format(old_len - len(round_token_counts)), file=sys.stderr)
+			print("Trimmed {} empty round(s) from session \"{}\".".format(old_len - len(round_token_counts), dyad_id), file=sys.stderr)
 
 		round_token_type_data = game_round_token_type_counts(round_token_counts)
 		yield dyad_id, round_token_type_data
@@ -99,11 +110,13 @@ def _count_utt_tokens(utts: Iterable[utterances.Utterance]):
 
 
 def __create_argparser():
-	result = argparse.ArgumentParser(description="Count ratios of new token types occurring.")
+	result = argparse.ArgumentParser(description="Count token/type counts of new token types occurring.")
 	result.add_argument("token_group_file", metavar="TOKEN_GROUP_FILEPATH",
 						help="The path to the token group mapping file to use.")
 	result.add_argument("inpaths", metavar="INPATH", nargs='+',
 						help="The paths to search for sessions to process.")
+	result.add_argument("-r", "--ratios", action="store_true",
+						help="Print type/token ratios rather than the individual counts of each.")
 	return result
 
 
@@ -119,7 +132,11 @@ def __main(args):
 
 	named_sessions = walk_session_data(args.inpaths)
 	outfile = sys.stdout
-	__print_ratios(session_round_token_type_data(named_sessions, token_groups.keys()), outfile)
+	token_type_data = session_round_token_type_data(named_sessions, token_groups.keys())
+	if args.ratios:
+		__print_ratios(token_type_data, outfile)
+	else:
+		__print_token_type_counts(token_type_data, outfile)
 
 
 def __print_ratios(token_type_data, outfile):
@@ -127,15 +144,48 @@ def __print_ratios(token_type_data, outfile):
 	max_round_count = max(
 		round_token_type_data.round_count for _, round_token_type_data in ordered_session_round_token_type_data)
 	round_col_idx_offset = len(__COLS_BEFORE_ROUND_COLS)
+	row_cell_count = max_round_count + round_col_idx_offset
 	round_colnames = ("R{}".format(i) for i in range(1, max_round_count + 1))
 	print(COL_DELIM.join(itertools.chain(__COLS_BEFORE_ROUND_COLS, round_colnames)), file=outfile)
 	for dyad_id, round_token_type_data in ordered_session_round_token_type_data:
-		row = [NULL_VALUE_REPR] * (max_round_count + round_col_idx_offset)
+		row = [NULL_VALUE_REPR] * row_cell_count
 		row[0] = dyad_id
 		for col_idx, token_type_counts in enumerate(round_token_type_data.round_token_type_counts,
 													start=round_col_idx_offset):
-			round_token_type_ratio = Decimal(token_type_counts[1]) / Decimal(token_type_counts[0])
+			round_token_type_ratio = Decimal(token_type_counts.types) / Decimal(token_type_counts.tokens)
 			row[col_idx] = __create_rounded_decimal_repr(round_token_type_ratio)
+		print(COL_DELIM.join(row), file=outfile)
+
+
+def __print_token_type_counts(token_type_data, outfile):
+	ordered_session_round_token_type_data = tuple(sorted(token_type_data, key=lambda item: item[0]))
+	max_round_count = max(
+		round_token_type_data.round_count for _, round_token_type_data in ordered_session_round_token_type_data)
+	round_col_idx_offset = len(__COLS_BEFORE_ROUND_COLS)
+	round_datapoint_vector_size = 2
+	row_cell_count = ((max_round_count * round_datapoint_vector_size) + round_col_idx_offset)
+	header_cells = [NULL_VALUE_REPR] * row_cell_count
+	header_idx = 0
+	for prev_col_name in __COLS_BEFORE_ROUND_COLS:
+		header_cells[header_idx] = prev_col_name
+		header_idx += 1
+	for round_id in range(1, max_round_count + 1):
+		header_cells[header_idx] = "R{}_TOKENS".format(round_id)
+		header_idx += 1
+		header_cells[header_idx] = "R{}_TYPES".format(round_id)
+		header_idx += 1
+	print(COL_DELIM.join(header_cells), file=outfile)
+
+	for dyad_id, round_token_type_data in ordered_session_round_token_type_data:
+		row = [NULL_VALUE_REPR] * row_cell_count
+		row[0] = dyad_id
+
+		row_idx = round_col_idx_offset
+		for token_type_counts in round_token_type_data.round_token_type_counts:
+			row[row_idx] = str(token_type_counts.tokens)
+			row_idx += 1
+			row[row_idx] = str(token_type_counts.types)
+			row_idx += 1
 		print(COL_DELIM.join(row), file=outfile)
 
 
