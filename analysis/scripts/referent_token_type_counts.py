7c47eea3b7b188dd283f4c9c2975a866e05ee417
errantlinguist <errantlinguist+github@gmail.com>
2017-09-12 22:35:58 +0200

    improved docstring

--- analysis/scripts/referent_token_type_counts.py
+++ analysis/scripts/referent_token_type_counts.py
@@ -76,7 +76,7 @@ class ReferentTokenTypeDatum(object):
 		self.round_data = tuple(
 			(round_id, self.__add_round(token_counts, self.total_data)) for round_id, token_counts in
 			round_token_counts)
-		"""A tuple of :class:`re_token_type_counts.RoundTokenTypeDatum` instances, each of which representing token counts for 
+		"""A tuple of pairs of round IDs with their corresponding :class:`re_token_type_counts.RoundTokenTypeDatum` instances, each of which representing token counts for 
 		a given round. They are in the same order as the rounds occur in the game."""
 
 	def __getitem__(self, round_id: Any):
