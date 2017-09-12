bc3880e700a3a24503e54fb6a2fa1ba20c88bb29
errantlinguist <errantlinguist+github@gmail.com>
2017-09-13 00:30:16 +0200

    tried to separate speakers

--- analysis/scripts/coreference_chain_overlap.py
+++ analysis/scripts/coreference_chain_overlap.py
@@ -3,17 +3,106 @@
 import argparse
 import re
 import statistics
+import string
 import sys
-from typing import Any, Dict, Iterable, Tuple
+from collections import defaultdict
+from typing import Any, Callable, Dict, Iterable, Tuple, TypeVar
 
+import game_events
 import re_token_type_counts
 import referent_token_type_counts
+import session_data as sd
+import token_groups as tg
 import utterances
-from session_data import walk_session_data
-from token_groups import read_token_group_dict
 
 COL_DELIM = '\t'
 NULL_VALUE_REPR = '?'
+
+T = TypeVar('T')
+
+
+
+class ReferentCounts(object):
+	def __init__(self):
+		self.participant_round_counts = defaultdict(list)
+		"""participant_id -> counts for entity for each round in which the entity is referenced for utterances by a particular participant"""
+		self.participant_total_counts = defaultdict(re_token_type_counts.FilteredTokenCountDatum)
+		"""participant_id -> total counts for entity for the entire coreference chain for utterances by a particular participant"""
+		self.round_total_counts = []
+		"""Counts for entity for each round in which the entity is referenced, for all utterances by all speakers"""
+		self.total_counts = re_token_type_counts.FilteredTokenCountDatum()
+		"""Total counts for entity for the entire coreference chain, for all utterances by all speakers"""
+
+	def __repr__(self, *args, **kwargs):
+		return self.__class__.__name__ + str(self.__dict__)
+
+
+class ParticipantCoreferenceChainTokenCounter(object):
+	def __init__(self, seg_utt_factory: utterances.SegmentUtteranceFactory,
+				 filtering_token_counter: Callable[
+					 [Iterable[utterances.Utterance]], re_token_type_counts.FilteredTokenCountDatum]):
+		self.seg_utt_factory = seg_utt_factory
+		self.filtering_token_counter = filtering_token_counter
+
+	def __call__(self, named_sessions: Iterable[Tuple[str, sd.SessionData]]) -> Dict[str, Dict[int, ReferentCounts]]:
+		result = {}
+		for dyad_id, session in named_sessions:
+			print("Processing session \"{}\".".format(dyad_id), file=sys.stderr)
+
+			events = game_events.read_events(session)
+			game_rounds = iter(game_events.create_game_rounds(events))
+			segments = utterances.read_segments(session.utts)
+			utt_times = utterances.UtteranceTimes(self.seg_utt_factory(segments))
+			game_round_utts = referent_token_type_counts.zip_game_round_utterances(game_rounds, utt_times)
+			participant_id_factory = ParticipantIdFactory()
+
+			entity_referent_counts = defaultdict(ReferentCounts)
+			for (round_id, (game_round, game_round_utts)) in enumerate(game_round_utts, start=1):
+				initial_event = next(iter(game_round.events))
+				# Get the participant ID for the first event's submitter without using it so that the first instructor of a game is always assigned the first participant ID (e.g. "A")
+				participant_id_factory(initial_event.submitter)
+				speaker_utts = utterances.create_speaker_dict(game_round_utts)
+
+				total_token_counts = self.filtering_token_counter(game_round_utts)
+				participant_token_counts = {}
+				for speaker_id, utts in speaker_utts.items():
+					speaker_participant_id = participant_id_factory(speaker_id)
+					participant_token_counts[speaker_participant_id] = self.filtering_token_counter(utts)
+
+				for entity_id, _ in initial_event.referent_entities:
+					referent_counts = entity_referent_counts[entity_id]
+					referent_counts.round_total_counts.append((round_id, total_token_counts))
+					# Update total counts for the entire coreference chain, for all utterances by all speakers
+					referent_counts.total_counts.update(total_token_counts)
+
+
+					for participant_id, participant_counts in participant_token_counts.items():
+						participant_round_counts = referent_counts.participant_round_counts[participant_id]
+						participant_round_counts.append((round_id, participant_counts))
+
+						participant_total_counts = referent_counts.participant_total_counts[participant_id]
+						participant_total_counts.update(participant_counts)
+
+			result[dyad_id] = entity_referent_counts
+
+		return result
+
+
+class ParticipantIdFactory(object):
+	def __init__(self, participant_id_symbols: Iterable[T] = string.ascii_uppercase):
+		self.participant_ids = {}
+		self.participant_id_symbol_iter = iter(participant_id_symbols)
+
+	def __call__(self, key: Any) -> T:
+		try:
+			result = self.participant_ids[key]
+		except KeyError:
+			result = self.__next_participant_id_symbol()
+			self.participant_ids[key] = result
+		return result
+
+	def __next_participant_id_symbol(self):
+		return next(self.participant_id_symbol_iter)
 
 
 class TokenTypeDataPrinter(object):
@@ -32,8 +121,7 @@ class TokenTypeDataPrinter(object):
 	def __init__(self, strict: bool):
 		self.strict = strict
 
-	def __call__(self, session_referent_token_counts: Iterable[
-		Tuple[Any, Dict[int, referent_token_type_counts.ReferentTokenTypeDatum]]], outfile):
+	def __call__(self, session_referent_token_counts: Iterable[Tuple[str, Dict[int, ReferentCounts]]], outfile):
 		print(COL_DELIM.join(
 			("DYAD", "ENTITY", "SEQUENCE_ORDER", "ROUND", "ROUND_TOKENS", "CUMULATIVE_TOKENS", "PREVIOUS_MEAN_TOKENS",
 			 "LENGTH_DROP",
@@ -43,7 +131,7 @@ class TokenTypeDataPrinter(object):
 		ordered_session_referent_token_counts = sorted(session_referent_token_counts, key=lambda item: item[0])
 		for dyad_id, referent_token_counts in ordered_session_referent_token_counts:
 			for entity_id, entity_token_counts in sorted(referent_token_counts.items(), key=lambda item: item[0]):
-				enumerated_round_counts = enumerate(entity_token_counts.round_counts_by_round_id(), start=1)
+				#entity_token_counts.
 				sequence_order, (round_id, round_token_counts) = next(enumerated_round_counts)
 				current_round_token_counts = round_token_counts.round_data.relevant_tokens
 				current_round_total_tokens = current_round_token_counts.total_token_count()
@@ -99,13 +187,16 @@ class TokenTypeDataPrinter(object):
 				str(overlap_ratio))
 
 
-def length_drop(current_total_tokens: int, mean_previous_token_count: int) -> float:
+def length_drop(current_total_tokens: int, mean_previous_token_count: float) -> float:
 	"""
 	See Aina, L. et al (2017) "Referring Expressions and Communicative Success in Task-oriented Dialogues", p. 9.
 
 	:param current_total_tokens: The total tokens uttered in the current unit under examination (e.g. either a single utterance or an entire sub-dialogue).
+	:type current_total_tokens: int
 	:param mean_previous_token_count:  The mean tokens uttered of the previous units under examination (e.g. either a single utterance or an entire sub-dialogue).
+	:type mean_previous_token_count: float
 	:return: A metric between -1 and 1.
+	:rtype float
 	"""
 	return (mean_previous_token_count - current_total_tokens) / (mean_previous_token_count + current_total_tokens)
 
@@ -130,21 +221,22 @@ def __main(args):
 	if group_regex:
 		group_pattern = re.compile(group_regex)
 		print("Calculating counts for token groups matching pattern \"{}\".".format(group_pattern), file=sys.stderr)
-		token_groups = read_token_group_dict(token_group_file_path,
-											 lambda group: group_pattern.match(group) is not None)
+		token_groups = tg.read_token_group_dict(token_group_file_path,
+												lambda group: group_pattern.match(group) is not None)
 	else:
-		token_groups = read_token_group_dict(token_group_file_path)
+		token_groups = tg.read_token_group_dict(token_group_file_path)
 	print("Read group info for {} token type(s).".format(len(token_groups)), file=sys.stderr)
 
-	named_sessions = walk_session_data(args.inpaths)
+	named_sessions = sd.walk_session_data(args.inpaths)
 	outfile = sys.stdout
-	referent_token_counter = referent_token_type_counts.ReferentTokenCounter(utterances.SegmentUtteranceFactory(),
-																			 re_token_type_counts.FilteringTokenTypeCounter(
-																				 lambda
-																					 token: token in token_groups.keys()))
-	referent_token_counts = referent_token_counter(named_sessions)
+	referent_token_counter = ParticipantCoreferenceChainTokenCounter(
+		utterances.SegmentUtteranceFactory(),
+		re_token_type_counts.FilteringTokenCounter(
+			lambda
+				token: token in token_groups.keys()))
+	session_entity_counts = referent_token_counter(named_sessions)
 	printer = TokenTypeDataPrinter(args.strict)
-	printer(referent_token_counts.items(), outfile)
+	printer(session_entity_counts.items(), outfile)
 
 
 if __name__ == "__main__":
