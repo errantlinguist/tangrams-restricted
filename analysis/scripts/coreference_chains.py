b11167df43e818f738ea59464880e8c08f2c0fe1
errantlinguist <errantlinguist+github@gmail.com>
2017-09-24 02:16:24 +0200

    removed buggy sort which actually tie-breaks with the lowest coref ID rather than the highest

--- analysis/scripts/coreference_chains.py
+++ analysis/scripts/coreference_chains.py
@@ -164,8 +164,7 @@ class DialogueCoreferenceChainDatum(Generic[C]):
 
 
 				if all_other_preceding_corefs:
-					all_other_preceding_corefs.sort(key=lambda coref: coref.coref_id)
-					directly_preceding_other_coref = max(all_other_preceding_corefs, default=None, key=lambda coref: coref.round_id)
+					directly_preceding_other_coref = max(all_other_preceding_corefs, default=None, key=lambda coref: (coref.round_id, coref.coref_id))
 					preceding_token_types = directly_preceding_other_coref.token_types
 					overlap = alignment_metrics.token_type_overlap_ratio(round_own_coref.token_types,
 																		 preceding_token_types)
