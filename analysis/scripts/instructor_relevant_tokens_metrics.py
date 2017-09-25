64bf9b8bd5cd99e5c2030732cb38fd9bd932dbee
Todd Shore <tcshore@kth.se>
2017-09-25 14:47:39 +0200

    code cleanup + type hints

--- analysis/scripts/instructor_relevant_tokens_metrics.py
+++ analysis/scripts/instructor_relevant_tokens_metrics.py
@@ -10,18 +10,25 @@ import pandas as pd
 
 CELL_MULTIVALUE_DELIM_PATTERN = re.compile("\\s*,\\s*")
 
+
 def parse_set(cell_value: str) -> FrozenSet[str]:
 	return frozenset(CELL_MULTIVALUE_DELIM_PATTERN.split(cell_value))
 
-def token_type_overlap(dyad):
-	own_relevant_tokens = dyad["RELEVANT_TOKENS_REFERENT"]
-	prev_tokens = dyad.shift(-1)["RELEVANT_TOKENS_REFERENT"]
-	union = own_relevant_tokens.union(prev_tokens)
-	overlap = own_relevant_tokens.intersection(prev_tokens)
-	return len(overlap) / len(union)
+
+# def token_type_overlap(dyad):
+#	own_relevant_tokens = dyad["RELEVANT_TOKENS_REFERENT"]
+#	prev_tokens = dyad.shift(-1)["RELEVANT_TOKENS_REFERENT"]
+#	union = own_relevant_tokens.union(prev_tokens)
+#	overlap = own_relevant_tokens.intersection(prev_tokens)
+#	return len(overlap) / len(union)
 
 
-
+def token_type_overlap(x: pd.DataFrame) -> pd.Series:
+	return (x.Tokens.str.len() -
+			x.Tokens.diff().str.len()) \
+		   / pd.Series([len(k[0].union(k[1]))
+						for k in zip(x.Tokens, x.Tokens.shift(1).fillna(''))],
+					   index=x.index)  # the for loop is part of this huge line
 
 
 def __create_argparser():
@@ -42,19 +49,15 @@ def __main(args):
 	# https://stackoverflow.com/a/46402641/1391325
 	tokens = round_tokens.RELEVANT_TOKENS_REFERENT
 	round_token_overlaps = round_tokens.assign(RELEVANT_TOKENS_REFERENT=tokens)
-	round_token_overlaps = round_token_overlaps.groupby(['DYAD', 'INSTRUCTOR']).apply( \
-		lambda x: (x.RELEVANT_TOKENS_REFERENT.str.len() -
-				   x.RELEVANT_TOKENS_REFERENT.diff().str.len()) \
-				  / pd.Series([len(k[0].union(k[1]))
-							   for k in zip(x.RELEVANT_TOKENS_REFERENT, x.RELEVANT_TOKENS_REFERENT.shift(1).fillna(''))],
-							  index=x.index))  # the for loop is part of this huge line
+	round_token_overlaps = round_token_overlaps.groupby(['DYAD', 'INSTRUCTOR']).apply(token_type_overlap)
 
 	round_token_overlaps = round_token_overlaps.reset_index(level=[0, 1], name='TokenOverlap')
-	round_token_overlaps = round_token_overlaps.assign(ROUND=round_tokens.ROUND, RELEVANT_TOKENS_REFERENT=round_tokens.RELEVANT_TOKENS_REFERENT)
+	round_token_overlaps = round_token_overlaps.assign(ROUND=round_tokens.ROUND,
+													   RELEVANT_TOKENS_REFERENT=round_tokens.RELEVANT_TOKENS_REFERENT)
 	round_token_overlaps = round_token_overlaps.sort_values(['DYAD', 'ROUND', 'INSTRUCTOR']).fillna('(no value)')
-	round_token_overlaps = round_token_overlaps[['DYAD', 'INSTRUCTOR', 'ROUND', 'RELEVANT_TOKENS_REFERENT', 'TokenOverlap']]
+	round_token_overlaps = round_token_overlaps[
+		['DYAD', 'INSTRUCTOR', 'ROUND', 'RELEVANT_TOKENS_REFERENT', 'TokenOverlap']]
 	print(round_token_overlaps)
-
 
 
 if __name__ == "__main__":
