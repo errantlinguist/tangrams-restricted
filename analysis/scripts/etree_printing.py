d84a03b57f5247484fc7e197e3b6a107860fa439
errantlinguist <errantlinguist+github@gmail.com>
2017-09-25 23:50:55 +0200

    removed intermediate tuple variable

--- analysis/scripts/etree_printing.py
+++ analysis/scripts/etree_printing.py
@@ -5,12 +5,11 @@ from typing import IO
 
 
 def print_etree_to_file(etree, encoding: str, outfile: IO[str]):
-	tmpfile = tempfile.mkstemp(text=True)
-	tmpfile_path = tmpfile[1]
+	tmpfile, tmpfile_path = tempfile.mkstemp(text=True)
 	try:
 		etree.write(tmpfile_path, encoding=encoding, xml_declaration=True, pretty_print=True)
 		with open(tmpfile_path, 'r', encoding=encoding) as inf:
 			print(inf.read(), file=outfile)
 	finally:
-		os.close(tmpfile[0])
+		os.close(tmpfile)
 		os.remove(tmpfile_path)
