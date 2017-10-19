8db0570ed49d8465ccc8ccbea62f87de073ab534
Todd Shore <tcshore@kth.se>
2017-10-19 11:19:02 +0200

    implemented "hashCode()", "equals(..)" and "toString()" so that it behaves properly when caching intermediate results

--- analysis/src/main/java/se/kth/speech/coin/tangrams/analysis/features/ImageEdgeCounter.java
+++ analysis/src/main/java/se/kth/speech/coin/tangrams/analysis/features/ImageEdgeCounter.java
@@ -60,7 +60,9 @@ public final class ImageEdgeCounter implements ToIntFunction<String> {
 		this(createPropValMap(loadEdgeCountProps()));
 	}
 
-	public ImageEdgeCounter(final Object2IntMap<String> resourceEdgeCounts) { // NO_UCD (use private)
+	public ImageEdgeCounter(final Object2IntMap<String> resourceEdgeCounts) { // NO_UCD
+																				// (use
+																				// private)
 		this.resourceEdgeCounts = resourceEdgeCounts;
 	}
 
@@ -81,6 +83,60 @@ public final class ImageEdgeCounter implements ToIntFunction<String> {
 		final int result = resourceEdgeCounts.getInt(resourceName);
 		assert result != resourceEdgeCounts.defaultReturnValue();
 		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof ImageEdgeCounter)) {
+			return false;
+		}
+		final ImageEdgeCounter other = (ImageEdgeCounter) obj;
+		if (resourceEdgeCounts == null) {
+			if (other.resourceEdgeCounts != null) {
+				return false;
+			}
+		} else if (!resourceEdgeCounts.equals(other.resourceEdgeCounts)) {
+			return false;
+		}
+		return true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (resourceEdgeCounts == null ? 0 : resourceEdgeCounts.hashCode());
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		final StringBuilder builder = new StringBuilder((resourceEdgeCounts.size() + 1) * 16);
+		builder.append("ImageEdgeCounter [resourceEdgeCounts=");
+		builder.append(resourceEdgeCounts);
+		builder.append("]");
+		return builder.toString();
 	}
 
 }
