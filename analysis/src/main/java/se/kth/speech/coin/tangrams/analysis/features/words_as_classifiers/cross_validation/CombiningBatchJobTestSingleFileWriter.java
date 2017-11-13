0ece49cb598cb67d4f13f45947bd5af317fd5577
Todd Shore <tcshore@kth.se>
2017-11-13 14:34:10 +0100

    moved class "TestParameters" inside "CombiningBatchJobTestSingleFileWriter"

--- analysis/src/main/java/se/kth/speech/coin/tangrams/analysis/features/words_as_classifiers/cross_validation/CombiningBatchJobTestSingleFileWriter.java
+++ analysis/src/main/java/se/kth/speech/coin/tangrams/analysis/features/words_as_classifiers/cross_validation/CombiningBatchJobTestSingleFileWriter.java
@@ -645,6 +645,162 @@ final class CombiningBatchJobTestSingleFileWriter { // NO_UCD (unused code)
 
 	}
 
+	private static class TestParameters {
+
+		private final Set<Cleaning> cleaning;
+
+		private final TokenFiltering tokenFiltering;
+
+		private final Tokenization tokenization;
+
+		private final TokenType tokenType;
+
+		private final Training trainingMethod;
+
+		private final Map<WordClassifierTrainingParameter, Object> trainingParams;
+
+		TestParameters(final Set<Cleaning> cleaning, final Tokenization tokenization, final TokenType tokenType,
+				final TokenFiltering tokenFiltering, final Training trainingMethod,
+				final Map<WordClassifierTrainingParameter, Object> trainingParams) {
+			this.cleaning = cleaning;
+			this.tokenization = tokenization;
+			this.tokenType = tokenType;
+			this.tokenFiltering = tokenFiltering;
+			this.trainingMethod = trainingMethod;
+			this.trainingParams = trainingParams;
+		}
+
+		/*
+		 * (non-Javadoc)
+		 *
+		 * @see java.lang.Object#equals(java.lang.Object)
+		 */
+		@Override
+		public boolean equals(final Object obj) {
+			if (this == obj) {
+				return true;
+			}
+			if (obj == null) {
+				return false;
+			}
+			if (!(obj instanceof TestParameters)) {
+				return false;
+			}
+			final TestParameters other = (TestParameters) obj;
+			if (cleaning == null) {
+				if (other.cleaning != null) {
+					return false;
+				}
+			} else if (!cleaning.equals(other.cleaning)) {
+				return false;
+			}
+			if (tokenFiltering != other.tokenFiltering) {
+				return false;
+			}
+			if (tokenType != other.tokenType) {
+				return false;
+			}
+			if (tokenization != other.tokenization) {
+				return false;
+			}
+			if (trainingMethod != other.trainingMethod) {
+				return false;
+			}
+			if (trainingParams == null) {
+				if (other.trainingParams != null) {
+					return false;
+				}
+			} else if (!trainingParams.equals(other.trainingParams)) {
+				return false;
+			}
+			return true;
+		}
+
+		/**
+		 * @return the cleaning
+		 */
+		public Set<Cleaning> getCleaning() {
+			return cleaning;
+		}
+
+		/**
+		 * @return the tokenFiltering
+		 */
+		public TokenFiltering getTokenFiltering() {
+			return tokenFiltering;
+		}
+
+		/**
+		 * @return the tokenization
+		 */
+		public Tokenization getTokenization() {
+			return tokenization;
+		}
+
+		/**
+		 * @return the tokenType
+		 */
+		public TokenType getTokenType() {
+			return tokenType;
+		}
+
+		/**
+		 * @return the trainingMethod
+		 */
+		public Training getTrainingMethod() {
+			return trainingMethod;
+		}
+
+		/**
+		 * @return the trainingParams
+		 */
+		public Map<WordClassifierTrainingParameter, Object> getTrainingParams() {
+			return trainingParams;
+		}
+
+		/*
+		 * (non-Javadoc)
+		 *
+		 * @see java.lang.Object#hashCode()
+		 */
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + (cleaning == null ? 0 : cleaning.hashCode());
+			result = prime * result + (tokenFiltering == null ? 0 : tokenFiltering.hashCode());
+			result = prime * result + (tokenType == null ? 0 : tokenType.hashCode());
+			result = prime * result + (tokenization == null ? 0 : tokenization.hashCode());
+			result = prime * result + (trainingMethod == null ? 0 : trainingMethod.hashCode());
+			result = prime * result + (trainingParams == null ? 0 : trainingParams.hashCode());
+			return result;
+		}
+
+		/*
+		 * (non-Javadoc)
+		 *
+		 * @see java.lang.Object#toString()
+		 */
+		@Override
+		public String toString() {
+			final StringBuilder builder = new StringBuilder(512);
+			builder.append("TestParameters [cleaning=");
+			builder.append(cleaning);
+			builder.append(", tokenFiltering=");
+			builder.append(tokenFiltering);
+			builder.append(", tokenization=");
+			builder.append(tokenization);
+			builder.append(", tokenType=");
+			builder.append(tokenType);
+			builder.append(", trainingMethod=");
+			builder.append(trainingMethod);
+			builder.append(", trainingParams=");
+			builder.append(trainingParams);
+			builder.append("]");
+			return builder.toString();
+		}
+	}
+
 	static final class TestException extends RuntimeException {
 
 		/**
