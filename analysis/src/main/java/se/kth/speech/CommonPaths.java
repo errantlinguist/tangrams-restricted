8eae60c7d85d145905ea097eef5225c5e357d06f
Todd Shore <tcshore@kth.se>
2017-11-13 10:52:17 +0100

    fixed comment formatting

--- analysis/src/main/java/se/kth/speech/CommonPaths.java
+++ analysis/src/main/java/se/kth/speech/CommonPaths.java
@@ -51,28 +51,22 @@ public final class CommonPaths {
 		final StringBuilder resultBuilder = new StringBuilder(maxPrefixLength);
 		final String[][] folders = new String[paths.length][];
 		for (int i = 0; i < paths.length; i++) {
-			folders[i] = paths[i].split(CANONICAL_PATH_SEPARATOR_PATTERN); // split
-																			// on
-																			// file
-																			// separator
+			// split on file separator
+			folders[i] = paths[i].split(CANONICAL_PATH_SEPARATOR_PATTERN);
 		}
 		for (int j = 0; j < folders[0].length; j++) {
-			final String thisFolder = folders[0][j]; // grab the next folder
-														// name in the first
-														// path
-			boolean allMatched = true; // assume all have matched in case there
-										// are no more paths
-			for (int i = 1; i < folders.length && allMatched; i++) { // look at
-																		// the
-																		// other
-																		// paths
+			// grab the next folder name in the first path
+			final String thisFolder = folders[0][j];
+			// assume all have matched in case there are no more paths
+			boolean allMatched = true;
+			// look at the other paths
+			for (int i = 1; i < folders.length && allMatched; i++) {
 				if (folders[i].length < j) { // if there is no folder here
 					allMatched = false; // no match
 					break; // stop looking because we've gone as far as we can
 				}
-				// otherwise
-				allMatched &= folders[i][j].equals(thisFolder); // check if it
-																// matched
+				// otherwise check if it matched
+				allMatched &= folders[i][j].equals(thisFolder);
 			}
 			if (allMatched) { // if they all matched this folder name
 				// add it to the answer
