ba2d4f1c372a9add9fa7161ccb9329d6799a98c8
errantlinguist <errantlinguist+github@gmail.com>
2017-11-19 16:45:14 +0100

    added copyright notice headers and Javadoc class comments

--- client/src/test/java/se/kth/speech/coin/tangrams/game/PatternMoveFactoryTest.java
+++ client/src/test/java/se/kth/speech/coin/tangrams/game/PatternMoveFactoryTest.java
@@ -1,3 +1,19 @@
+/*
+ *  This file is part of client.
+ *
+ *  client is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package se.kth.speech.coin.tangrams.game;
 
 import java.util.ArrayList;
@@ -22,6 +38,12 @@ import se.kth.speech.SpatialMatrix;
 import se.kth.speech.SpatialRegion;
 import se.kth.speech.coin.tangrams.MatrixTests;
 
+/**
+ *
+ * @author <a href="mailto:tcshore@kth.se">Todd Shore</a>
+ * @since 30 Mar 2017
+ * 
+ */
 @RunWith(Theories.class)
 public final class PatternMoveFactoryTest {
 
