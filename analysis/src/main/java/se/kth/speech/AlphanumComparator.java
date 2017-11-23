e4c8417912328b4853e2d27da00bb186c8bad149
errantlinguist <errantlinguist+github@gmail.com>
2017-11-23 21:27:54 +0100

    removed redundant final modifiers

--- analysis/src/main/java/se/kth/speech/AlphanumComparator.java
+++ analysis/src/main/java/se/kth/speech/AlphanumComparator.java
@@ -139,7 +139,7 @@ public final class AlphanumComparator implements Comparator<String> {
 	 * Length of string is passed in for improved efficiency (only need to
 	 * calculate it once)
 	 **/
-	private final String getChunk(final String s, final int slength, int marker) {
+	private String getChunk(final String s, final int slength, int marker) {
 		final StringBuilder chunk = new StringBuilder();
 		char c = s.charAt(marker);
 		chunk.append(c);
@@ -166,7 +166,7 @@ public final class AlphanumComparator implements Comparator<String> {
 		return chunk.toString();
 	}
 
-	private final boolean isDigit(final char ch) {
+	private boolean isDigit(final char ch) {
 		return ch >= 48 && ch <= 57;
 	}
 }
