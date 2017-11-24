d6df413a2dbef9f5329d156ece77b6325a6606bc
Todd Shore <tcshore@kth.se>
2017-11-24 14:11:21 +0100

    removed Weka from project

--- analysis/src/main/java/se/kth/speech/coin/tangrams/analysis/features/EntityFeature.java
+++ analysis/src/main/java/se/kth/speech/coin/tangrams/analysis/features/EntityFeature.java
@@ -19,18 +19,13 @@ package se.kth.speech.coin.tangrams.analysis.features;
 import java.awt.Color;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.EnumMap;
-import java.util.EnumSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
-import java.util.function.Function;
 import java.util.function.ToIntFunction;
 
 import se.kth.speech.IntArrays;
 import se.kth.speech.SpatialRegion;
 import se.kth.speech.coin.tangrams.content.ImageVisualizationInfo;
-import weka.core.Attribute;
 
 /**
  * @author <a href="mailto:tcshore@kth.se">Todd Shore</a>
@@ -165,49 +160,6 @@ public enum EntityFeature {
 				builder.append(']');
 				return builder.toString();
 			}
-		}
-
-		private static final String DEFAULT_ATTR_NAME_PREFIX = "";
-
-		public static Map<EntityFeature, Attribute> createFeatureAttrMap(final List<String> shapeVals) {
-			return createFeatureAttrMap(DEFAULT_ATTR_NAME_PREFIX, shapeVals);
-		}
-
-		private static Map<EntityFeature, Attribute> createFeatureAttrMap(final Iterable<EntityFeature> features,
-				final String attrNamePrefix, final List<String> shapeVals) {
-			final Map<EntityFeature, Function<String, Attribute>> attrFactories = createFeatureTypedAttrFactoryMap(
-					shapeVals);
-			final Map<EntityFeature, Attribute> result = new EnumMap<>(EntityFeature.class);
-			for (final EntityFeature feature : features) {
-				final Function<String, Attribute> attrFactory = attrFactories.get(feature);
-				result.put(feature, attrFactory.apply(attrNamePrefix + feature.name()));
-			}
-			return result;
-		}
-
-		private static Map<EntityFeature, Attribute> createFeatureAttrMap(final String attrNamePrefix,
-				final List<String> shapeVals) {
-			return createFeatureAttrMap(EnumSet.allOf(EntityFeature.class), attrNamePrefix, shapeVals);
-		}
-
-		private static Map<EntityFeature, Function<String, Attribute>> createFeatureTypedAttrFactoryMap(
-				final List<String> shapeVals) {
-			final Map<EntityFeature, Function<String, Attribute>> result = new EnumMap<>(EntityFeature.class);
-			final Function<String, Attribute> doubleVal = name -> new Attribute(name);
-			result.put(EntityFeature.ALPHA, doubleVal);
-			result.put(EntityFeature.BLUE, doubleVal);
-			result.put(EntityFeature.BRIGHTNESS, doubleVal);
-			result.put(EntityFeature.EDGE_COUNT, doubleVal);
-			result.put(EntityFeature.GREEN, doubleVal);
-			result.put(EntityFeature.HUE, doubleVal);
-			result.put(EntityFeature.POSITION_X, doubleVal);
-			result.put(EntityFeature.POSITION_Y, doubleVal);
-			result.put(EntityFeature.RED, doubleVal);
-			result.put(EntityFeature.SATURATION, doubleVal);
-			result.put(EntityFeature.SHAPE, name -> new Attribute(name, shapeVals));
-			result.put(EntityFeature.SIZE, doubleVal);
-			assert result.size() == EntityFeature.values().length;
-			return result;
 		}
 
 		@Override
