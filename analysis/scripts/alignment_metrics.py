19a92a800914635c922564cd0cd15e99e3f03cfe
errantlinguist <errantlinguist+github@gmail.com>
2017-12-03 19:09:21 +0100

    added description and licensing info

--- analysis/scripts/alignment_metrics.py
+++ analysis/scripts/alignment_metrics.py
@@ -1,3 +1,13 @@
+"""
+Metrics used for analyzing alignment of dialogue participant language throughout the course of a dialogue.
+
+See Shore, T. et al. (2018) "Towards a common dataset for research on conceptual pacts and alignment in task-oriented dialogue". Submitted to LREC 2018, Eleventh International Conference on Language Resources and Evaluation.
+"""
+
+__author__ = "Todd Shore <errantlinguist+github@gmail.com>"
+__copyright__ = "Copyright (C) 2016-2017 Todd Shore"
+__license__ = "GNU General Public License, Version 3"
+
 from decimal import Decimal
 from typing import FrozenSet
 
