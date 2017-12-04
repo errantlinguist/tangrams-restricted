bf1c947bf2e897cc70536bb5013c6bad047e73a8
Todd Shore <tcshore@kth.se>
2017-12-04 14:19:50 +0100

    Now printing header to concatenated file

--- analysis/scripts/cat_utts.sh
+++ analysis/scripts/cat_utts.sh
@@ -1,11 +1,24 @@
 #!/bin/sh
 
+exit_code=1
+
 if [ $# -lt 1 ]
 then
 	echo "Usage: $0 INPATHS..."
 	exit_code=64
 else
-	find "$@" -type f -name "utts.tsv" -exec tail -n+2 {} \;
+	inpaths="$@"
+	header=`find "${inpaths}" -type f -name "utts.tsv" -exec head -n1 {} \; | uniq`
+	header_linecount=`echo "${header}" | wc -l`
+	if [ ${header_linecount} -gt 1 ]
+	then
+		echo "More than one unique header found! Aborting." 1>&2
+	else
+		echo "${header}"
+		find "${inpaths}" -type f -name "utts.tsv" -exec tail -n+2 {} \;
+	fi
+	
+
 fi
 
 exit ${exit_code}
