5779de3c16c4838e55f96477bdd955f7b2a52259
Todd Shore <tcshore@kth.se>
2017-12-04 14:24:22 +0100

    Added documentation and licensing info

--- analysis/scripts/cat_utts.sh
+++ analysis/scripts/cat_utts.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 
+# This file is part of se.kth.speech.coin.tangrams-restricted.analysis.
+#
+# tangrams is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# A script for concatenating all tabular utterance files (named "utts.xml").
+# Author: Todd Shore <errantlinguist+github@gmail.com>
+# Since: 2017-12-04
+
 exit_code=1
 
 if [ $# -lt 1 ]
