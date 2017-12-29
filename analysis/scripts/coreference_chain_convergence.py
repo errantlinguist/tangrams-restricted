b84c3ed3893ab9f9205f4f9759f7e3887a88866e
errantlinguist <errantlinguist+github@gmail.com>
2017-12-29 15:45:22 +0100

    made code object-oriented

--- analysis/scripts/coreference_chain_convergence.py
+++ analysis/scripts/coreference_chain_convergence.py
@@ -10,37 +10,40 @@ __license__ = "GNU General Public License, Version 3"
 
 import argparse
 import sys
-from typing import Tuple
+from typing import Optional, Tuple
 
 import pandas as pd
 
 import session_data as sd
 import utterances
 
-EVENT_DF_DYAD_ID_COL_NAME = "DYAD"
-EVENT_DF_TOKEN_SEQ_COL_NAME = utterances.UtteranceTabularDataColumn.TOKEN_SEQ.value
 
+class SessionRoundTokenBagDataFrameFactory(object):
+	DYAD_ID_COL_NAME = "DYAD"
+	TOKEN_SEQ_COL_NAME = utterances.UtteranceTabularDataColumn.TOKEN_SEQ.value
 
-def concatenate_token_seqs(df: pd.DataFrame) -> Tuple[str, ...]:
-	# noinspection PyProtectedMember
-	row_dicts = (row._asdict() for row in df.itertuples(index=False))
-	token_seqs = (row_dict[utterances.UtteranceTabularDataColumn.TOKEN_SEQ.value] for row_dict in row_dicts)
-	return tuple(token for token_seq in token_seqs for token in token_seq)
+	@staticmethod
+	def __concatenate_token_seqs(df: pd.DataFrame) -> Tuple[str, ...]:
+		# noinspection PyProtectedMember
+		row_dicts = (row._asdict() for row in df.itertuples(index=False))
+		token_seqs = (row_dict[utterances.UtteranceTabularDataColumn.TOKEN_SEQ.value] for row_dict in row_dicts)
+		return tuple(token for token_seq in token_seqs for token in token_seq)
 
+	def __init__(self, utt_reader: Optional[utterances.UtteranceTabularDataReader] = None):
+		self.utt_reader = utterances.UtteranceTabularDataReader() if utt_reader is None else utt_reader
 
-def read_session_utterances(session_data: sd.SessionData,
-							utt_reader: utterances.UtteranceTabularDataReader) -> pd.DataFrame:
-	session_name = session_data.name
-	print("Reading events and utterances for \"{}\".".format(session_name), file=sys.stderr)
-	utts_df = utt_reader(session_data.utts)
-	round_utts = utts_df.groupby(utterances.UtteranceTabularDataColumn.ROUND_ID.value)
-	round_token_bags = dict((round_id, concatenate_token_seqs(group)) for round_id, group in round_utts)
+	def __call__(self, session_data: sd.SessionData) -> pd.DataFrame:
+		session_name = session_data.name
+		print("Reading events and utterances for \"{}\".".format(session_name), file=sys.stderr)
+		utts_df = self.utt_reader(session_data.utts)
+		round_utts = utts_df.groupby(utterances.UtteranceTabularDataColumn.ROUND_ID.value)
+		round_token_bags = dict((round_id, self.__concatenate_token_seqs(group)) for round_id, group in round_utts)
 
-	result = session_data.read_events()
-	result[EVENT_DF_DYAD_ID_COL_NAME] = session_name
-	result[EVENT_DF_TOKEN_SEQ_COL_NAME] = result[sd.DataColumn.ROUND_ID.value.name].transform(
-		lambda round_id: round_token_bags[round_id])
-	return result
+		result = session_data.read_events()
+		result[self.DYAD_ID_COL_NAME] = session_name
+		result[self.TOKEN_SEQ_COL_NAME] = result[sd.DataColumn.ROUND_ID.value.name].transform(
+			lambda round_id: round_token_bags[round_id])
+		return result
 
 
 def __create_argparser() -> argparse.ArgumentParser:
@@ -54,11 +57,12 @@ def __create_argparser() -> argparse.ArgumentParser:
 def __main(args):
 	inpaths = args.inpaths
 	print("Looking for session data underneath {}.".format(inpaths), file=sys.stderr)
-	utt_reader = utterances.UtteranceTabularDataReader()
-	session_utt_df = pd.concat(
-		read_session_utterances(session_data, utt_reader) for _, session_data in sd.walk_session_data(inpaths))
+	df_factory = SessionRoundTokenBagDataFrameFactory()
+	session_utt_df = pd.concat(df_factory(session_data) for _, session_data in sd.walk_session_data(inpaths))
 	print("DF shape is {}; {} unique dyad(s).".format(session_utt_df.shape,
-													  len(session_utt_df[EVENT_DF_DYAD_ID_COL_NAME].unique())), file=sys.stderr)
+													  len(session_utt_df[
+															  SessionRoundTokenBagDataFrameFactory.DYAD_ID_COL_NAME].unique())),
+		  file=sys.stderr)
 
 
 if __name__ == "__main__":
