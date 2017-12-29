971cab0f8b1b2307f79d017435a65bb02de09f64
errantlinguist <errantlinguist+github@gmail.com>
2017-12-29 13:21:16 +0100

    added method doctring

--- analysis/scripts/etree_printing.py
+++ analysis/scripts/etree_printing.py
@@ -1,5 +1,5 @@
 """
-Utilities for printing ElementTree instances to file-like objects in a manner agnostic to the actual ElementTree implementation used, e.g. standard Python's xml.etree.ElementTree.ElementTree vs lxml's lxml.etree.
+Utilities for printing ElementTree instances to file-like objects in a manner agnostic to the actual ElementTree implementation used, e.g. standard Python's xml.etree.ElementTree.ElementTree vs. lxml's lxml.etree.
 
 see https://docs.python.org/3/library/xml.etree.elementtree.html
 """
@@ -16,6 +16,13 @@ from typing import IO
 
 def print_etree_to_file(etree, encoding: str, outfile: IO[str]):
 	tmpfile, tmpfile_path = tempfile.mkstemp(text=True)
+	"""
+	Prints an ElementTree-like object to file in a manner which is agnostic towards the XML implementation used.
+	
+	:param etree: An ElementTree-like object, e.g. either standard Python's xml.etree.ElementTree.ElementTree or lxml's lxml.etree.
+	:param encoding: The output file encoding.
+	:param outfile: The file-like object to write to.
+	"""
 	try:
 		etree.write(tmpfile_path, encoding=encoding, xml_declaration=True, pretty_print=True)
 		with open(tmpfile_path, 'r', encoding=encoding) as inf:
