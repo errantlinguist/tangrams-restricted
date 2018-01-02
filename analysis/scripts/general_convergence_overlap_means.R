271bf0d43ae748e53dbdbb0ab0ad14bf380bc3e4
Todd Shore <tcshore@kth.se>
2018-01-02 16:21:26 +0100

    started script for calculating means, Pearson's R and significance of general convergence

--- analysis/scripts/general_convergence_overlap_means.R
+++ analysis/scripts/general_convergence_overlap_means.R
@@ -31,14 +31,54 @@ if (!file_test("-f", infile))
 # Correlations with significance levels
 library(Hmisc)
 
-coref_overlaps <- read.table(infile, sep="\t", header=TRUE)
-print(sprintf("Read %d coreference overlap value(s).", nrow(coref_overlaps)), quote=FALSE)
+filterInvalidCorefs <- function(corefOverlaps, minCorefChainLength, maxCorefChainLength) {
+  origSampleSize <- nrow(corefOverlaps)
+  corefOverlaps[!corefOverlaps$seq<minCorefChainLength,] -> result
+  sizeAfterMinPrune <- nrow(result)
+  print(sprintf("Removed %d observation(s) lesser than coref sequence ordinality %d.", origSampleSize - sizeAfterMinPrune, minCorefChainLength), quote=FALSE)
+  result[!result$seq>maxCorefChainLength,] -> result
+  sizeAfterMaxPrune <- nrow(result)
+  print(sprintf("Removed %d observation(s) greater than coref sequence ordinality %d.", sizeAfterMinPrune - sizeAfterMaxPrune, maxCorefChainLength), quote=FALSE)
+  return(result)
+}
 
+testIntervalCorefSeqs <- function(corefOverlaps) {
+  print("Pearson correlation coefficient:", quote=FALSE)
+  print(rcorr(corefOverlaps$seq, corefOverlaps$overlap, type="pearson"))
+  
+  # https://www.rdocumentation.org/packages/stats/versions/3.4.3/topics/cor.test
+  cor.test(corefOverlaps$seq, corefOverlaps$overlap, method="pearson", exact = TRUE, conf.level=0.999)
+}
+
+testOrdinalCorefSeqs <- function(corefOverlaps) {
+  print("Converting coreference sequence ordinalities to ordinal values.", quote=FALSE)
+  corefOverlaps <- cbind(corefOverlaps)
+  corefOverlaps$seq <- as.ordered(corefOverlaps$seq)
+  
+  print("Spearman's rank correlation coefficient:", quote=FALSE)
+  print(rcorr(corefOverlaps$seq, corefOverlaps$overlap, type="spearman"))
+  
+  # https://www.rdocumentation.org/packages/stats/versions/3.4.3/topics/cor.test
+  #cor.test(corefOverlaps$seq, corefOverlaps$overlap, method = "spearman", exact = TRUE)
+  cor.test(corefOverlaps$seq, corefOverlaps$overlap, method = "spearman", exact = TRUE)
+  #cor.test(corefOverlaps$seq, corefOverlaps$overlap, alternative = c("two.sided", "less", "greater"), method = "kendall", exact = TRUE, continuity = FALSE)
+}
+
+# https://stat.ethz.ch/R-manual/R-devel/library/base/html/options.html
+options(na.action=na.fail)
+
+print(sprintf("Reading data from \"%s\".", infile), quote=FALSE)
+corefOverlaps <- read.table(infile, sep="\t", header=TRUE)
+print(sprintf("Read %d coreference overlap value(s).", nrow(corefOverlaps)), quote=FALSE)
+corefOverlaps <- filterInvalidCorefs(corefOverlaps, 2, 6)
 
 # https://stackoverflow.com/a/2677859/1391325
 se <- function(x) sqrt(var(x)/length(x))
 
 print("Coference overlap means:", quote=FALSE)
-aggs <- aggregate(coref_overlaps$overlap, list(seq=coref_overlaps$seq), function(x) c(mean = mean(x), sd = sd(x), sem = se(x)))
+aggs <- aggregate(corefOverlaps$overlap, list(seq=corefOverlaps$seq), function(x) c(mean = mean(x), sd = sd(x), sem = se(x)))
 # https://stackoverflow.com/a/21509371/1391325
-round(aggs, 3)
+round(aggs, 4)
+
+testIntervalCorefSeqs(corefOverlaps)
+testOrdinalCorefSeqs(corefOverlaps)
